40449e54ca803dc7307cdddda1b4d9b5aeaa8182
Nax <max@bacoux.com>
2022-08-11 03:24:40 +0200

    Compression & cache

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'yaz0', '~> 0.4'
+gem 'thread'
--- lib/combo/compressor.rb
+++ lib/combo/compressor.rb
@@ -1,4 +1,6 @@
+require 'digest'
 require 'yaz0'
+require 'thread/pool'
 require 'combo/common'
 require 'combo/dma_data'
 
@@ -10,21 +12,16 @@ module Combo::Compressor
     new_data = "\x00" * (32 * 1024 * 1024)
 
     puts "Compressing #{game}..."
+    files = compress_files(data, dma_old, dma_new)
     paddr = 0
-    dma_old.each do |i, e_old|
-      next if e_old.dummy?
-      e_new = dma_new.entry(i)
-      src_paddr = e_new.pstart
-      src_plen = e_new.plen
-      d = data[src_paddr, src_plen]
-      if e_old.compressed?
-        d = Yaz0.compress(d).force_encoding('ASCII-8BIT')
-      end
+    dma_old.each do |i, e|
+      next if e.dummy?
+      d = files[i]
       size = (d.size + 15) & 0xfffffff0
-      if e_old.compressed?
+      if e.compressed?
         dma_new.set_compressed(i, paddr, size)
       else
-        dma_new.write(i, e_old.vstart, e_old.vend, paddr, 0)
+        dma_new.write(i, e.vstart, e.vend, paddr, 0)
       end
       new_data[paddr, d.length] = d
       paddr = paddr + size
@@ -34,5 +31,32 @@ module Combo::Compressor
     new_data[meta[:dma_addr], meta[:dma_count] * 0x10] = dma_new.data
 
     new_data
+  end
+
+  def self.compress_files(data, dma_old, dma_new)
+    pool = Thread.pool(32)
+    files = Array.new(dma_old.count)
+    dma_new.each do |i, e_new|
+      next if e_new.dummy?
+      pool.process do
+        e_old = dma_old.entry(i)
+        d = data[e_new.pstart, e_new.plen]
+        if e_old.compressed?
+          hash = Digest::SHA1.hexdigest(d)
+          cache_dir = File.join(Combo::PATH_BUILD, "cache", "yaz0", hash[0,2])
+          path = File.join(cache_dir, hash[2..])
+          if File.exists?(path)
+            d = File.binread(path)
+          else
+            d = Yaz0.compress(d).force_encoding('ASCII-8BIT')
+            FileUtils.mkdir_p(cache_dir)
+            File.binwrite(path, d)
+          end
+        end
+        files[i] = d
+      end
+    end
+    pool.shutdown
+    files
   end
 end
