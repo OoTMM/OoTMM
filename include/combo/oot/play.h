ed3143e559c1a89bff587864827081b8d0d24b79
Nax <max@bacoux.com>
2022-08-21 03:47:04 +0200

    Working kaleido_scope changes

--- include/combo/oot/play.h
+++ include/combo/oot/play.h
@@ -5,13 +5,60 @@
 
 typedef struct PACKED ALIGNED(4)
 {
-    GameState    gs;
-    u16          sceneId;
-    char         unk_000a6[0xfde2];
-    char         textBuffer[4]; /* Real size unknown */
-    char         unk_0fe8c[0x1918];
-    char         objTable[4]; /* Real size unknown */
-    char         unk_117a8[0xd70];
+    char    unk_0[0x128];
+    void*   icon_item;
+    void*   icon_item_24;
+    void*   icon_item_s;
+    void*   icon_item_lang;
+    void*   name_texture;
+    void*   unk_13c;
+    char    unk_140[0x94];
+    u16     state;
+    char    unk_1d6[0xe];
+    u16     changing;
+    u16     screen_prev_idx;
+    u16     screen_idx;
+    char    unk_1ea[0x2e];
+    s16     item_cursor;
+    char    unk_21a[2];
+    s16     quest_cursor;
+    s16     equip_cursor;
+    s16     map_cursor;
+    s16     item_x;
+    char    unk_05_[0x0004];
+    s16     equipment_x;
+    char    unk_06_[0x0002];
+    s16     item_y;
+    char    unk_07_[0x0004];
+    s16     equipment_y;
+    char    unk_08_[0x0004];
+    s16     cursor_pos;
+    char    unk_09_[0x0002];
+    s16     item_id;
+    s16     item_item;
+    s16     map_item;
+    s16     quest_item;
+    s16     equip_item;
+    char    unk_0A_[0x0004];
+    s16     quest_hilite;
+    char    unk_0B_[0x0018];
+    s16     quest_song;
+    char    unk_0C_[0x0016];
+    char    s27C[0x0038];
+}
+PauseContext;
+
+typedef struct PACKED ALIGNED(4)
+{
+    GameState       gs;
+    u16             sceneId;
+    char            unk_000a6[0xfde2];
+    char            textBuffer[4]; /* Real size unknown */
+    char            unk_0fe8c[0x8d4];
+    PauseContext    pauseCtx;
+    char            unk_10a14[0xd90];
+    char            objTable[4]; /* Real size unknown */
+    char            unk_117a8[0xd70];
 }
 GameState_Play;
 
--- src/oot/ovl/kaleido_scope.c
+++ src/oot/ovl/kaleido_scope.c
@@ -1,19 +1,86 @@
 #include <combo.h>
 
+u32 popcount(u32 x)
+{
+    x = (x & 0x55555555) + ((x >> 1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+    x = (x & 0x0f0f0f0f) + ((x >> 4) & 0x0f0f0f0f);
+    x = (x & 0x00ff00ff) + ((x >> 8) & 0x00ff00ff);
+    x = (x & 0x0000ffff) + ((x >> 16) & 0x0000ffff);
+    return x;
+}
+
+static void toggleOcarina(void)
+{
+    u8* slot;
+
+    slot = gOotSave.inventory + 7;
+    if (*slot == ITEM_OOT_FAIRY_OCARINA)
+    {
+        *slot = ITEM_OOT_OCARINA_OF_TIME;
+    }
+    else
+    {
+        *slot = ITEM_OOT_FAIRY_OCARINA;
+    }
+}
+
+static int checkItemToggle(GameState_Play* play)
+{
+    PauseContext* p;
+    int ret;
+    int press;
+
+    p = &play->pauseCtx;
+    ret = 0;
+    press = !!(play->gs.input[0].pressed.buttons & 0x20);
+
+    if (p->item_cursor == 7 && (popcount(gComboSave.ootOcarinas) >= 2))
+    {
+        ret = 1;
+        if (press)
+            toggleOcarina();
+    }
+
+    return ret;
+}
+
 void KaleidoSetCursorColor(GameState_Play* play)
 {
+    PauseContext* p;
     u8 r;
     u8 g;
     u8 b;
 
+    p = &play->pauseCtx;
     r = 0xff;
     g = 0xff;
     b = 0xff;
 
-    /* Hack: Check for L button presses */
-    if (play->gs.input[0].pressed.buttons & 0x20)
+    /* Not on Z/R */
+    if (p->cursor_pos == 0)
     {
-        gOotSave.inventory[1]++;
+        switch (p->screen_idx)
+        {
+        case 0:
+            /* Item select */
+            r = 0xff;
+            g = 0xff;
+            b = 0x00;
+            if (checkItemToggle(play))
+            {
+                r = 0x00;
+                g = 0xff;
+                b = 0xff;
+            }
+            break;
+        case 3:
+            /* Equipment */
+            r = 0x00;
+            g = 0x00;
+            b = 0xff;
+            break;
+        }
     }
 
     OPEN_DISPS(play->gs.gfx);
