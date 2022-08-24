48896499568360d50ba990e3b6a5af4267e8f19b
Nax <max@bacoux.com>
2022-08-24 11:19:07 +0200

    Skip most entrance cutscenes

--- src/payload/oot/ovl/play.c
+++ src/payload/oot/ovl/play.c
@@ -3,8 +3,73 @@
 extern void Play_Init(void*);
 extern void comboSwitchToMM(void);
 
+static void skipEntranceCutscene(GameState_Play* play)
+{
+    switch (gSave.entrance)
+    {
+    case 0x0185:
+        SetEventChk(0xa0);
+        break;
+    case 0x013d:
+        SetEventChk(0xa1);
+        break;
+    case 0x00db:
+        SetEventChk(0xa3);
+        break;
+    case 0x0108:
+        SetEventChk(0xa4);
+        break;
+    case 0x0138:
+        if (gSave.age == 1)
+            SetEventChk(0xa5);
+        else
+            SetEventChk(0xba);
+        break;
+    case 0x014d:
+        SetEventChk(0xa6);
+        break;
+    case 0x0053:
+        SetEventChk(0xa7);
+        break;
+    case 0x0000:
+        SetEventChk(0xa8);
+        break;
+    case 0x0102:
+        SetEventChk(0xb1);
+        break;
+    case 0x0117:
+        SetEventChk(0xb2);
+        break;
+    case 0x0129:
+        SetEventChk(0xb3);
+        break;
+    case 0x0157:
+        SetEventChk(0xb4);
+        break;
+    case 0x0028:
+        SetEventChk(0xb5);
+        break;
+    case 0x00E4:
+        SetEventChk(0xb6);
+        break;
+    case 0x0225:
+        SetEventChk(0xb7);
+        break;
+    case 0x0123:
+        SetEventChk(0xb8);
+        break;
+    case 0x0147:
+        SetEventChk(0xb9);
+        break;
+    default:
+        break;
+    }
+}
+
 void hookPlay_Init(GameState_Play* play)
 {
+    skipEntranceCutscene(play);
+
     if ((gSave.entrance & 0xfffc) == 0x0530)
     {
         comboGameSwitch();
