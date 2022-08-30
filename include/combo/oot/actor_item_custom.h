b90071cee157063e5e12d2b7174010af67c798ce
Nax <max@bacoux.com>
2022-08-30 19:08:41 +0200

    Use a custom actor for freestanding items

---
+++ include/combo/oot/actor_item_custom.h
@@ -0,0 +1,15 @@
+#ifndef COMBO_OOT_ACTOR_ITEM_CUSTOM_H
+#define COMBO_OOT_ACTOR_ITEM_CUSTOM_H
+
+#include <combo/oot/actor.h>
+
+typedef struct
+{
+    Actor base;
+    s16   gi;
+    u8    type;
+    u8    flag;
+}
+Actor_ItemCustom;
+
+#endif
