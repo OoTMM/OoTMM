fe15a17ba18c72ad51474776165f0f58db69cfaf
Nax <max@bacoux.com>
2022-09-11 22:14:32 +0200

    TS: Add compression cache

--- lib/compress.ts
+++ lib/compress.ts
@@ -1,10 +1,40 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { Readable } from 'stream';
+import crypto from 'crypto';
 import { createYaz0Stream } from 'yaz0';
 
 import { Game, CONFIG, PATH_BUILD } from './config';
 import { DmaData } from './dma';
+import { fileExists } from './util';
+
+export const compressFile = async (data: Buffer): Promise<Buffer> => {
+  const hash = crypto.createHash('sha256').update(data).digest('hex');
+  const dir = path.resolve(PATH_BUILD, 'cache', 'yaz0', hash.slice(0, 2));
+  const filename = path.resolve(dir, hash);
+
+  /* Check for the file in cache */
+  await fs.mkdir(dir, { recursive: true });
+  if (await fileExists(filename)) {
+    return fs.readFile(filename);
+  }
+
+  /* Cache miss - compress */
+  return new Promise((resolve, reject) => {
+    const stream = createYaz0Stream('compress', { size: data.length, level: 7 });
+    const chunks: Buffer[] = [];
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => {
+      const out = Buffer.concat(chunks);
+      fs.writeFile(filename, out).then(_ => resolve(out));
+    });
+    const bufStream = new Readable();
+    bufStream.push(data);
+    bufStream.push(null);
+    bufStream.pipe(stream);
+  });
+};
 
 const compressFiles = async (rom: Buffer, dmaOld: DmaData) => {
   const promises: Promise<Buffer>[] = [];
@@ -24,18 +54,7 @@ const compressFiles = async (rom: Buffer, dmaOld: DmaData) => {
     };
 
     /* We need to compress */
-    promises.push(new Promise((resolve, reject) => {
-      const stream = createYaz0Stream('compress', { size: entryOld.virtEnd - entryOld.virtStart, level: 8 });
-      const chunks: Buffer[] = [];
-      stream.on('data', (chunk) => chunks.push(chunk));
-      stream.on('end', () => resolve(Buffer.concat(chunks)));
-      stream.on('error', reject);
-
-      const bufStream = new Readable();
-      bufStream.push(rom.subarray(entryOld.virtStart, entryOld.virtEnd));
-      bufStream.push(null);
-      bufStream.pipe(stream);
-    }));
+    promises.push(compressFile(rom.subarray(entryOld.virtStart, entryOld.virtEnd)));
   }
   return Promise.all(promises);
 };
