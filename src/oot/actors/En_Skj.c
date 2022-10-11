fabb4ddcab44f2038ee96f4fc06aba1d52070e03
Nax <max@bacoux.com>
2022-10-11 17:00:06 +0200

    OoT: Add Lost Woods Memory Game

--- src/oot/actors/En_Skj.c
+++ src/oot/actors/En_Skj.c
@@ -8,3 +8,13 @@ int EnSkj_GiveItem_TreeHP(Actor* actor, GameState_Play* play, s16 gi, float a, f
 
 PATCH_CALL(0x80a6e550, EnSkj_GiveItem_TreeHP);
 PATCH_CALL(0x80a6e5e8, EnSkj_GiveItem_TreeHP);
+
+int EnSkj_GiveItem_MemoryGame(Actor* actor, GameState_Play* play, s16 gi, float a, float b)
+{
+    if (gi == GI_OOT_HEART_PIECE)
+        gi = comboOverride(OV_NPC, 0, NPC_OOT_LOST_WOODS_MEMORY, gi);
+    return GiveItem(actor, play, gi, a, b);
+}
+
+PATCH_CALL(0x80a6f7dc, EnSkj_GiveItem_MemoryGame);
+PATCH_CALL(0x80a6f860, EnSkj_GiveItem_MemoryGame);
