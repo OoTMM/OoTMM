46e9b5b6c24e23c845da8686af51ea23db13a7f2
Nax <max@bacoux.com>
2022-10-11 18:22:59 +0200

    OoT: Add Magic Beans

---
+++ src/oot/actors/En_Ms.c
@@ -0,0 +1,11 @@
+#include <combo.h>
+
+int EnMs_GiveItem(Actor* actor, GameState_Play* play, s16 gi, float a, float b)
+{
+    gi = comboOverride(OV_NPC, 0, NPC_OOT_BEAN_SELLER, gi);
+    gSave.beans = 1;
+    return GiveItem(actor, play, gi, a, b);
+}
+
+PATCH_CALL(0x80aa08dc, EnMs_GiveItem);
+PATCH_CALL(0x80aa0998, EnMs_GiveItem);
