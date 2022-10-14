9b727d64cc19fc4d6f33b03951336341845134b7
Nax <max@bacoux.com>
2022-10-14 07:33:16 +0200

    OoT: Fully working treasure shop game

---
+++ src/oot/actors/Item/Item_Etcetera.c
@@ -0,0 +1,38 @@
+#include <combo.h>
+
+void ItemEtcetera_DrawTreasureGame(Actor* this, GameState_Play* play)
+{
+    s16 gi;
+
+    if (*(u8*)((char*)play + 0x1c27) == 0)
+        return;
+
+    switch (this->variable & 0xff)
+    {
+    case 0x8:
+        gi = GI_OOT_RUPEE_GREEN;
+        break;
+    case 0x9:
+        gi = GI_OOT_RUPEE_BLUE;
+        break;
+    case 0xa:
+        gi = GI_OOT_RUPEE_RED;
+        break;
+    case 0xb:
+        gi = GI_OOT_RUPEE_PURPLE;
+        break;
+    case 0xc:
+        gi = comboOverride(OV_CHEST, SCE_OOT_TREASURE_SHOP, 0x0a, GI_OOT_HEART_PIECE);
+        break;
+    case 0xd:
+        gi = GI_OOT_SMALL_KEY;
+        break;
+    default:
+        gi = GI_OOT_DEFENSE_UPGRADE;
+        break;
+    }
+
+    comboDrawGI(play, this, gi);
+}
+
+PATCH_FUNC(0x80a5e5b8, ItemEtcetera_DrawTreasureGame);
