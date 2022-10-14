f5c2af887006cfe15ac432fd686030ba9f4688f0
Nax <max@bacoux.com>
2022-10-14 16:50:04 +0200

    OoT: Add Shooting Gallery

---
+++ src/oot/actors/En/En_Syateki_Man.c
@@ -0,0 +1,20 @@
+#include <combo.h>
+
+int EnSyatekiMan_GiveItem(Actor* actor, GameState_Play* play, s16 gi, float a, float b)
+{
+    switch (gi)
+    {
+    case GI_OOT_BULLET_BAG2:
+    case GI_OOT_BULLET_BAG3:
+        gi = comboOverride(OV_NPC, 0, NPC_OOT_SHOOTING_GAME_CHILD, gi);
+        break;
+    case GI_OOT_QUIVER2:
+    case GI_OOT_QUIVER3:
+        gi = comboOverride(OV_NPC, 0, NPC_OOT_SHOOTING_GAME_ADULT, gi);
+        break;
+    }
+    return GiveItem(actor, play, gi, a, b);
+}
+
+PATCH_CALL(0x809a130c, EnSyatekiMan_GiveItem);
+PATCH_CALL(0x809a140c, EnSyatekiMan_GiveItem);
