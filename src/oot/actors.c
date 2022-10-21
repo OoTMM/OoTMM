5e1d872dd2e0f6eb5146fb0136995936a11bc66f
Nax <max@bacoux.com>
2022-10-21 10:41:22 +0200

    OoT: Remove actor blacklist, add hooks into En_Owl

--- src/oot/actors.c
+++ src/oot/actors.c
@@ -1,30 +1,7 @@
 #include <combo.h>
 
-typedef struct
-{
-    u16 sceneId;
-    u16 actorId;
-}
-RemovedActor;
-
-static RemovedActor kRemovedActors[] = {
-    { SCE_OOT_CASTLE_COURTYARD, 0x0a9 }, /* Zelda Courtyard - Impa */
-    { SCE_OOT_HYRULE_FIELD, AC_EN_OWL },
-    { SCE_OOT_HYRULE_CASTLE, AC_EN_OWL },
-    { SCE_OOT_ZORA_RIVER, AC_EN_OWL },
-    { SCE_OOT_LOST_WOODS, AC_EN_OWL },
-};
-
 Actor* hookSpawnActor(void* const_1, GameState_Play* play, s16 actorId, float x, float y, float z, s16 rx, s16 ry, s16 rz, u16 variable)
 {
-    for (size_t i = 0; i < ARRAY_SIZE(kRemovedActors); ++i)
-    {
-        if (kRemovedActors[i].sceneId == play->sceneId && kRemovedActors[i].actorId == actorId)
-        {
-            return NULL;
-        }
-    }
-
     switch (actorId)
     {
     case AC_ITEM00:
