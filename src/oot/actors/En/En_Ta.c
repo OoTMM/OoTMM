4f364cc91edf00370df8fff93ca7cc7bf3bf1b14
Nax <max@bacoux.com>
2022-10-21 11:02:04 +0200

    OoT: Talon fix

--- src/oot/actors/En/En_Ta.c
+++ src/oot/actors/En/En_Ta.c
@@ -1,5 +1,17 @@
 #include <combo.h>
 
+int EnTa_GetActiveItem_Chicken(Actor* this, GameState_Play* play)
+{
+    int item;
+
+    item = GetActiveItem(play);
+    if (item == 3)
+        comboRemoveTradeItemChild(XITEM_OOT_CHILD_CHICKEN);
+    return item;
+}
+
+PATCH_CALL(0x809223d4, EnTa_GetActiveItem_Chicken);
+
 int EnTa_GiveItem_MilkBottle(Actor* this, GameState_Play* play, s16 gi, float a, float b)
 {
     gi = comboOverride(OV_NPC, 0, NPC_OOT_TALON_BOTTLE, gi);
