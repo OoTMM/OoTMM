63ac46a7b77432d32caf52462edbc9052b9315b3
Nax <max@bacoux.com>
2022-10-23 08:01:12 +0200

    MM: Fix chests

--- src/mm/actors/En/En_Box.c
+++ src/mm/actors/En/En_Box.c
@@ -1,9 +1,9 @@
 #include <combo.h>
 
-int EnBox_GiveItem(Actor* actor, GameState_Play* play, s16 gi, float a, float b)
+int EnBox_GiveItemDefaultRange(Actor* actor, GameState_Play* play, s16 gi)
 {
     gi = comboOverride(OV_CHEST, play->sceneId, actor->variable & 0x1f, gi);
-    return GiveItem(actor, play, gi, a, b);
+    return GiveItemDefaultRange(actor, play, gi);
 }
 
-PATCH_CALL(0x80868fe0, EnBox_GiveItem);
+PATCH_CALL(0x80868fe0, EnBox_GiveItemDefaultRange);
