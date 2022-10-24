aa027745f1d5dc3af9cb9b3f93b238c70f4de690
Nax <max@bacoux.com>
2022-10-24 20:27:39 +0200

    MM: Disable Owl at Swamp

--- src/mm/save.c
+++ src/mm/save.c
@@ -1,6 +1,17 @@
 #include <combo.h>
 
+#define ENTRANCE_CLOCKTOWN  0xd800
+#define ENTRANCE_WOODS      0xc200
+#define ENTRANCE_DKING      0x7610
+#define ENTRANCE_TEMPLE1    0x3000
+#define ENTRANCE_BOSS1      0x3800
+#define ENTRANCE_SWAMP      0x8420
+
 void Sram_AfterOpenSave(void)
 {
-    gSave.entranceIndex = 0xd800;
+    gSave.entranceIndex = ENTRANCE_CLOCKTOWN;
+
+#if defined(DEBUG)
+    gSave.entranceIndex = ENTRANCE_SWAMP;
+#endif
 }
