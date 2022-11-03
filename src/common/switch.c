c8c4f57f501e58c66cc48ac32679ce449fb1a3bb
Nax <max@bacoux.com>
2022-11-03 20:01:56 +0100

    Common: Stability fixes

--- src/common/switch.c
+++ src/common/switch.c
@@ -11,6 +11,39 @@ typedef void (*EntryPoint)(void)  __attribute__ ((noreturn));
 #endif
 #define FOREIGN_DRAM   (0x80000000 | FOREIGN_OFF)
 
+static void waitSubsystems(void)
+{
+    u32 tmp;
+
+    for (;;)
+    {
+        tmp = IO_READ(SP_STATUS_REG);
+        if (tmp & 1)
+            break;
+    }
+
+    for (;;)
+    {
+        tmp = IO_READ(SP_DMA_BUSY_REG);
+        if (!(tmp & 1))
+            break;
+    }
+
+    for (;;)
+    {
+        tmp = IO_READ(DPC_STATUS_REG);
+        if (!(tmp & 0x170))
+            break;
+    }
+
+    for (;;)
+    {
+        tmp = IO_READ(AI_STATUS_REG);
+        if (!(tmp & 0x40000000))
+            break;
+    }
+}
+
 void comboGameSwitch(void)
 {
 #if defined(GAME_OOT)
@@ -19,6 +52,7 @@ void comboGameSwitch(void)
 
     comboWriteSave();
     comboDisableInterrupts();
+    waitSubsystems();
     osInvalICache((void*)FOREIGN_DRAM, 0x100000);
     osInvalDCache((void*)FOREIGN_DRAM, 0x100000);
     comboDma_NoCacheInval((void*)FOREIGN_OFF, FOREIGN_CART, 0x100000);
