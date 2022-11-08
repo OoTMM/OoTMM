9f8864dc145e14116e44890a719fda66619cc877
Nax <max@bacoux.com>
2022-11-08 17:10:38 +0100

    MM: Add Mayor HP

---
+++ src/mm/actors/En/En_Dt.c
@@ -0,0 +1,10 @@
+#include <combo.h>
+
+int EnDt_GiveItem(Actor* this, GameState_Play* play, s16 gi, float a, float b)
+{
+    gi = comboOverride(OV_NPC, 0, NPC_MM_MAYOR_HEART_PIECE, gi);
+    return GiveItem(this, play, gi, a, b);
+}
+
+PATCH_CALL(0x80beab14, EnDt_GiveItem);
+PATCH_CALL(0x80beabe0, EnDt_GiveItem);
