8cb906f80770a9718010e9e9dba3e92efba541b7
Nax <max@bacoux.com>
2022-11-14 01:01:33 +0100

    MM: Fix GetCollectItemBehavior

---
+++ src/mm/actors/En/En_Dekubaba.c
@@ -0,0 +1,31 @@
+#include <combo.h>
+
+void EnDekubaba_GiveItemDefaultRange(Actor* this, GameState_Play* play, s16 gi)
+{
+    float dist;
+
+    if (GetItemCollectBehavior(ITEM_MM_STICK) == 0xff)
+    {
+        GiveItemDefaultRange(this, play, gi);
+        return;
+    }
+
+    dist = this->xzDistanceFromLink;
+    if (dist < 0.f)
+        dist = -dist;
+    if (dist < 50.f)
+    {
+        dist = this->position.y - GET_LINK(play)->base.position.y;
+        if (dist < 0.f)
+            dist = -dist;
+        if (dist < 10.f)
+        {
+            this->attachedA = &GET_LINK(play)->base;
+            PlaySound(0x4824);
+            AddItem(play, ITEM_MM_STICK);
+        }
+    }
+}
+
+PATCH_CALL(0x808b3f38, EnDekubaba_GiveItemDefaultRange);
+PATCH_CALL(0x808f21f4, EnDekubaba_GiveItemDefaultRange); /* En_Karebaba */
