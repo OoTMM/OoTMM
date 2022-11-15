360766929a927c465d6d151512bee8b895cd415e
Nax <max@bacoux.com>
2022-11-15 10:58:19 +0100

    MM: All trade item removal fixed

--- src/mm/actors.c
+++ src/mm/actors.c
@@ -86,6 +86,9 @@ static void Actor_ByteCode_RemoveItem(s16 item, s16 slot)
     case ITEM_MM_PENDANT_OF_MEMORIES:
         comboRemoveTradeItem3(XITEM_MM_TRADE3_PENDANT_OF_MEMORIES);
         break;
+    case ITEM_MM_LETTER_TO_MAMA:
+        comboRemoveTradeItem2(XITEM_MM_TRADE2_LETTER_TO_MAMA);
+        break;
     default:
         RemoveItem(item, slot);
         break;
