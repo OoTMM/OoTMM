f27ac798aefa81e515ae58fe02d9029ad21892fa
Nax <max@bacoux.com>
2022-11-23 06:34:03 +0100

    TS: Add rom auto-swapping

--- lib/combo/decompress.ts
+++ lib/combo/decompress.ts
@@ -18,6 +18,42 @@ type DecompressedGame = {
   dma: Buffer,
 };
 
+const swapIfNecessary = (rom: Buffer) => {
+  const sig = rom.readUInt32BE(0) >>> 0;
+  let swap32 = false;
+  let swap16 = false;
+  switch (sig) {
+  case 0x80371240:
+    /* Big endian - no further work needed */
+    break;
+  case 0x40123780:
+    /* Little endian - swap 32-bit words */
+    swap32 = true;
+    break;
+  case 0x37804012:
+    /* Mixed endian - swap 16-bit words */
+    swap16 = true;
+    break;
+  case 0x12400387:
+    /* Mixed endian - swap 16-bit words and 32-bit words */
+    swap32 = true;
+    swap16 = true;
+    break;
+  default:
+    throw new Error(`Bad n64 rom signature ${sig.toString(16)}`);
+  }
+  if (swap32) {
+    for (let i = 0; i < rom.length; i += 4) {
+      rom.writeUInt32BE(rom.readUInt32LE(i), i);
+    }
+  }
+  if (swap16) {
+    for (let i = 0; i < rom.length; i += 2) {
+      rom.writeUInt16BE(rom.readUInt16LE(i), i);
+    }
+  }
+};
+
 const checkGameHash = (game: Game, rom: Buffer) => {
   const h = CRC32.buf(rom, 0) >>> 0;
   const hashes = CONFIG[game].crc32;
@@ -70,6 +106,7 @@ export type DecompressedRoms = {
 export const decompressGames = async (monitor: Monitor, params: DecompressGamesParams): Promise<DecompressedRoms> => {
   for (const g of GAMES) {
     monitor.log("Validating " + g);
+    swapIfNecessary(params[g]);
     checkGameHash(g, params[g]);
   }
   monitor.log("Decompressing");
