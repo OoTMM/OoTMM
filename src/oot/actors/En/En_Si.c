c9ed13402db1ecf5c456cdfbf783b4c4143e464f
Nax <max@bacoux.com>
2022-12-09 02:12:35 +0100

    OoT: Skullsanity graphics works

---
+++ src/oot/actors/En/En_Si.c
@@ -0,0 +1,30 @@
+#include <combo.h>
+
+static s16 EnSi_Override(Actor* this)
+{
+    u16 key;
+
+    /* Poor man's ctz */
+    switch (this->variable & 0xff)
+    {
+    case 0x01: key = 0x00; break;
+    case 0x02: key = 0x01; break;
+    case 0x04: key = 0x02; break;
+    case 0x08: key = 0x03; break;
+    case 0x10: key = 0x04; break;
+    case 0x20: key = 0x05; break;
+    case 0x40: key = 0x06; break;
+    case 0x80: key = 0x07; break;
+    default:
+        UNREACHABLE();
+    }
+    key += ((((this->variable + 0x100) >> 8) & 0x1f) * 8);
+    return comboOverride(OV_GS, 0, key, GI_OOT_GS_TOKEN);
+}
+
+void EnSi_Draw(Actor* this, GameState_Play* play)
+{
+    comboDrawGI(play, this, EnSi_Override(this), 0);
+}
+
+PATCH_CALL(0x80b4b3f8, EnSi_Draw);
