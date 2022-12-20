72b2956324109312eb94fb882c8c8bbbfc87d835
Nax <max@bacoux.com>
2022-12-20 20:04:44 +0100

    Common: Explicit Progressive hints

--- src/common/override.c
+++ src/common/override.c
@@ -61,7 +61,7 @@ static s32 comboOverrideRaw(u16 key)
     return -1;
 }
 
-static s16 comboOverrideImpl(u16 sceneId, u16 id, s16 gi)
+static s16 comboOverrideImpl(u16 sceneId, u16 id, s16 gi, int flags)
 {
     s32 override;
     u16 absGi;
@@ -73,12 +73,19 @@ static s16 comboOverrideImpl(u16 sceneId, u16 id, s16 gi)
     absGi = gi > 0 ? gi : -gi;
     if (override >= 0)
         absGi = override;
-    absGi = comboProgressive(absGi);
+
+    if (!(flags & OVF_NO_PROGRESSIVE))
+        absGi = comboProgressive(absGi);
 
     return gi > 0 ? absGi : -absGi;
 }
 
 s16 comboOverride(int type, u16 sceneId, u16 id, s16 gi)
+{
+    return comboOverrideEx(type, sceneId, id, gi, 0);
+}
+
+s16 comboOverrideEx(int type, u16 sceneId, u16 id, s16 gi, int flags)
 {
     switch (type)
     {
@@ -95,5 +102,5 @@ s16 comboOverride(int type, u16 sceneId, u16 id, s16 gi)
         sceneId = SCE_GS;
         break;
     }
-    return comboOverrideImpl(sceneId, id, gi);
+    return comboOverrideImpl(sceneId, id, gi, flags);
 }
