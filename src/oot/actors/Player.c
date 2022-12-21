eb32c2f7053927daff80577010907ac2f9372c57
Nax <max@bacoux.com>
2022-12-21 07:55:56 +0100

    OoT: Fix some dpad issues

--- src/oot/actors/Player.c
+++ src/oot/actors/Player.c
@@ -4,5 +4,11 @@ void Player_UpdateWrapper(Actor_Player* this, GameState_Play* play)
 {
     Player_Update(this, play);
     comboDpadUpdate(play);
-    comboDpadUse(play);
+}
+
+int Player_DpadHook(Actor_Player* this, GameState_Play* play)
+{
+    if (Player_UsingItem(this))
+        return 1;
+    return comboDpadUse(play);
 }
