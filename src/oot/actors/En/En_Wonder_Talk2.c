9f01806b4046fb9cb57bab2ea343ab478c2b67e0
Nax <max@bacoux.com>
2022-12-30 20:46:52 +0100

    OoT: Bugfix for some GTG challenges

--- src/oot/actors/En/En_Wonder_Talk2.c
+++ src/oot/actors/En/En_Wonder_Talk2.c
@@ -2,15 +2,8 @@
 
 void EnWonderTalk2_AfterInit(Actor* this, GameState_Play* play)
 {
-    u8 switchFlag;
-
     if ((this->variable & 0xf000) == 0x4000)
     {
-        switchFlag = this->variable & 0x3F;
-        if (switchFlag != 0x3F)
-        {
-            SetSwitchFlag(play, switchFlag);
-        }
         ActorDestroy(this);
     }
 }
