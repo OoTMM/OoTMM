4e381e8aeb183f280e81689b3f2ff3e46f2b822b
Nax <max@bacoux.com>
2023-01-06 21:28:50 +0100

    OoT: Skip owl travel cutscenes

--- src/oot/actors/En/En_Owl.c
+++ src/oot/actors/En/En_Owl.c
@@ -13,3 +13,24 @@ void EnOwl_AfterInit(Actor* this)
         break;
     }
 }
+
+static void EnOwl_StartCutscene(Actor* this, GameState_Play* play)
+{
+    TransitionContext* t;
+
+    t = &play->transition;
+    t->type = TRANS_TYPE_NORMAL;
+    t->gfx = TRANS_GFX_SHORTCUT;
+    switch ((this->variable & 0xfc0) >> 6)
+    {
+    case 7:
+        t->entrance = 0x27e;
+        break;
+    case 8:
+    case 9:
+        t->entrance = 0x554;
+        break;
+    }
+}
+
+PATCH_FUNC(0x80ab1414, EnOwl_StartCutscene);
