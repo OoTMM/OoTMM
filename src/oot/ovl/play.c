1533c9862cea85848df78d1577293b34e6bc6b5a
Nax <max@bacoux.com>
2023-01-06 19:01:19 +0100

    OoT: Add more entrance cutscenes skips

--- src/oot/ovl/play.c
+++ src/oot/ovl/play.c
@@ -83,69 +83,6 @@ static void debugCheat(GameState_Play* play)
 #endif
 }
 
-static void skipEntranceCutscene(GameState_Play* play)
-{
-    switch (gSave.entrance)
-    {
-    case 0x0185:
-        SetEventChk(0xa0);
-        break;
-    case 0x013d:
-        SetEventChk(0xa1);
-        break;
-    case 0x00db:
-        SetEventChk(0xa3);
-        break;
-    case 0x0108:
-        SetEventChk(0xa4);
-        break;
-    case 0x0138:
-        if (gSave.age == 1)
-            SetEventChk(0xa5);
-        else
-            SetEventChk(0xba);
-        break;
-    case 0x014d:
-        SetEventChk(0xa6);
-        break;
-    case 0x0053:
-        SetEventChk(0xa7);
-        break;
-    case 0x0000:
-        SetEventChk(0xa8);
-        break;
-    case 0x0102:
-        SetEventChk(0xb1);
-        break;
-    case 0x0117:
-        SetEventChk(0xb2);
-        break;
-    case 0x0129:
-        SetEventChk(0xb3);
-        break;
-    case 0x0157:
-        SetEventChk(0xb4);
-        break;
-    case 0x0028:
-        SetEventChk(0xb5);
-        break;
-    case 0x00E4:
-        SetEventChk(0xb6);
-        break;
-    case 0x0225:
-        SetEventChk(0xb7);
-        break;
-    case 0x0123:
-        SetEventChk(0xb8);
-        break;
-    case 0x0147:
-        SetEventChk(0xb9);
-        break;
-    default:
-        break;
-    }
-}
-
 static void eventFixes(GameState_Play* play)
 {
     /* Skip Zelda's cutscene when having all the spiritual stones */
@@ -234,7 +171,6 @@ void hookPlay_Init(GameState_Play* play)
     }
     comboObjectsReset();
     debugCheat(play);
-    skipEntranceCutscene(play);
     eventFixes(play);
 
     Play_Init(play);
