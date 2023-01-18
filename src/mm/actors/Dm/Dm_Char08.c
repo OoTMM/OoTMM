a4a4fd4c26dbabd7744a55872214467c436d73b5
Nax <max@bacoux.com>
2023-01-18 22:41:57 +0100

    MM: Skip the emerging turtle cutscene

--- src/mm/actors/Dm/Dm_Char08.c
+++ src/mm/actors/Dm/Dm_Char08.c
@@ -8,3 +8,24 @@ void DmChar08_WarpToTemple(Actor* this, GameState_Play* play)
 }
 
 PATCH_FUNC(0x80aafa18, DmChar08_WarpToTemple);
+
+void DmChar08_Emerge(GameState_Play* play, Actor* this)
+{
+    this->position.x = -6480.f;
+    this->position.z = 1750.f;
+    this->speedRot.x = 0;
+    this->speedRot.y = -0x7234;
+    this->speedRot.z = 0;
+    this->flags |= 1;
+
+    *(u16*)((char*)this + 0xbc) = 0;
+    *(u16*)((char*)this + 0xbe) = 0x8dcc;
+    *(u16*)((char*)this + 0xc0) = 0;
+
+    *((char*)this + 0x1ff) = 2;
+    *((char*)this + 0x202) = 2;
+    *((char*)this + 0x203) = 99;
+    *((char*)this + 0x200) = 1;
+    *((char*)this + 0x207) = 0;
+    *((char*)this + 0x208) = 0;
+}
