d4278a84b22312dc87279c31adeb8083ab1961c2
Nax <max@bacoux.com>
2023-01-19 14:36:45 +0100

    MM: Skip evan cutscene

--- src/mm/actors/En/En_Zos.c
+++ src/mm/actors/En/En_Zos.c
@@ -2,6 +2,9 @@
 
 int EnZos_GiveItem(Actor* this, GameState_Play* play, s16 gi, float a, float b)
 {
+    if (!(GET_LINK(play)->state & PLAYER_ACTOR_STATE_GET_ITEM))
+        Message_Close(play);
+
     if (gi == GI_MM_HEART_PIECE)
     {
         gi = comboOverride(OV_NPC, 0, NPC_MM_ZORA_EVAN, gi);
