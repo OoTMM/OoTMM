6a0cae2918527f7e00be803355714283a1ce1070
Nax <max@bacoux.com>
2023-01-22 18:41:57 +0100

    Add CI for stable/dev deploys

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,17 @@
 
 rm -rf vendor/core
 mkdir -p vendor/core
-git clone --depth=50 --branch=develop https://github.com/OoTMM/core vendor/core || exit 1
-cd vendor/core
-export VERSION="$(git rev-parse --short HEAD)"
+if [ "stable" -eq "$1" ]; then
+  git clone --depth=50 --branch=master https://github.com/OoTMM/core vendor/core || exit 1
+  tag="$(git tag --list --sort='-version:refname' | head -n1)"
+  git checkout "$tag"
+  cd vendor/core
+  export VERSION="$tag"
+else
+  git clone --depth=50 --branch=develop https://github.com/OoTMM/core vendor/core || exit 1
+  cd vendor/core
+  export VERSION="$(git rev-parse --short HEAD)"
+fi
 npm i || exit 1
 npm run build || exit 1
 cd ../..
