88e3a21ded39e5330a8aedede2814e43ec762f23
Nax <max@bacoux.com>
2023-01-22 18:48:10 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,11 @@
 
 rm -rf vendor/core
 mkdir -p vendor/core
-if [ "stable" -eq "$1" ]; then
+if [ "stable" = "$1" ]; then
   git clone --depth=50 --branch=master https://github.com/OoTMM/core vendor/core || exit 1
-  tag="$(git tag --list --sort='-version:refname' | head -n1)"
-  git checkout "$tag"
   cd vendor/core
-  export VERSION="$tag"
+  export VERSION="$(git tag --list --sort='-version:refname' | head -n1)"
+  git checkout "$VERSION"
 else
   git clone --depth=50 --branch=develop https://github.com/OoTMM/core vendor/core || exit 1
   cd vendor/core
