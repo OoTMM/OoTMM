ff805de2e6cd7885e4c23cfdec7eb3b91adff6db
Nax <max@bacoux.com>
2023-01-24 22:52:29 +0100

    OoT: Fix owls

--- src/oot/actors/En/En_Owl.c
+++ src/oot/actors/En/En_Owl.c
@@ -18,18 +18,21 @@ static void EnOwl_StartCutscene(Actor* this, GameState_Play* play)
 {
     TransitionContext* t;
 
-    t = &play->transition;
-    t->type = TRANS_TYPE_NORMAL;
-    t->gfx = TRANS_GFX_SHORTCUT;
-    switch ((this->variable & 0xfc0) >> 6)
+    if (this->xzDistanceFromLink < 50.f)
     {
-    case 7:
-        t->entrance = 0x27e;
-        break;
-    case 8:
-    case 9:
-        t->entrance = 0x554;
-        break;
+        t = &play->transition;
+        t->type = TRANS_TYPE_NORMAL;
+        t->gfx = TRANS_GFX_SHORTCUT;
+        switch ((this->variable & 0xfc0) >> 6)
+        {
+        case 7:
+            t->entrance = 0x27e;
+            break;
+        case 8:
+        case 9:
+            t->entrance = 0x554;
+            break;
+        }
     }
 }
 
