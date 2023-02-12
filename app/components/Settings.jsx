05c11f2f07ac543cb22e500af205b5df07d10a34
Alexandre Hamel <ahamel@landr.com>
2023-02-12 01:01:54 -0500

    make the dropdown pretty

--- app/components/Settings.jsx
+++ app/components/Settings.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import { SETTINGS } from '@ootmm/core';
+import React from "react";
+import { SETTINGS } from "@ootmm/core";
 
-import { Dropdown } from './Dropdown';
-import { Checkbox } from './Checkbox';
+import { Dropdown } from "./Dropdown";
+import { Checkbox } from "./Checkbox";
 
 export const Settings = ({ category, settings, setSetting }) => {
-  const settingsData = SETTINGS.filter(s => s.category === category);
+  const settingsData = SETTINGS.filter((s) => s.category === category);
+  const enumList = settingsData.filter((x) => x.type == "enum");
+  const booleanList = settingsData.filter((x) => x.type == "boolean");
 
   return (
-    <form className='settings'>
-      {settingsData.map(setting => {
-        if (setting.type === 'enum') {
-          return (
-            <Dropdown
-              value={settings[setting.key]}
-              key={setting.key}
-              label={setting.name}
-              options={setting.values}
-              onChange={v => setSetting({ [setting.key]: v }) }
-            />
-          );
-        } else if (setting.type === 'boolean') {
-          return (
-            <Checkbox
-              key={setting.key}
-              label={setting.name}
-              checked={settings[setting.key]}
-              onChange={v => setSetting({ [setting.key]: v }) }
-            />
-          );
-        } else {
-          return null;
-        }
-      })}
+    <form className="settings">
+      <div>
+        {enumList.map((setting) => (
+          <Dropdown
+            value={settings[setting.key]}
+            key={setting.key}
+            label={setting.name}
+            options={setting.values}
+            onChange={(v) => setSetting({ [setting.key]: v })}
+          />
+        ))}
+      </div>
+      <div>
+        {booleanList.map((setting) => (
+          <Checkbox
+            key={setting.key}
+            label={setting.name}
+            checked={settings[setting.key]}
+            onChange={(v) => setSetting({ [setting.key]: v })}
+          />
+        ))}
+      </div>
     </form>
   );
 };
