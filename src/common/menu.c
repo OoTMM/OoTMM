062940859f8932bbaa5f77fd738c8b108793eae6
Nax <max@bacoux.com>
2023-02-12 11:57:53 +0000

    Common: Make dungeon keys menu scrollable

--- src/common/menu.c
+++ src/common/menu.c
@@ -11,22 +11,33 @@ typedef struct
     int flags;
     int maxKeys;
 }
+DungeonDef;
+
+typedef struct
+{
+    /* data */
+}
 DungeonData;
 
-static DungeonData gDungeonData[] = {
-    { "Deku",       SCE_OOT_INSIDE_DEKU_TREE,       DD_OOT, 0 },
-    { "Dodongo",    SCE_OOT_DODONGO_CAVERN,         DD_OOT, 0 },
-    { "Jabu",       SCE_OOT_INSIDE_JABU_JABU,       DD_OOT, 0 },
-    { "Forest",     SCE_OOT_TEMPLE_FOREST,          DD_OOT, 0 },
-    { "Fire",       SCE_OOT_TEMPLE_FIRE,            DD_OOT, 0 },
-    { "Water",      SCE_OOT_TEMPLE_WATER,           DD_OOT, 0 },
-    { "Shadow",     SCE_OOT_TEMPLE_SHADOW,          DD_OOT, 0 },
-    { "Spirit",     SCE_OOT_TEMPLE_SPIRIT,          DD_OOT, 0 },
-    { "Well",       SCE_OOT_BOTTOM_OF_THE_WELL,     DD_OOT, 0 },
-    { "Ice",        SCE_OOT_ICE_CAVERN,             DD_OOT, 0 },
-    { "Hideout",    SCE_OOT_THIEVES_HIDEOUT,        DD_OOT, 0 },
-    { "GTG",        SCE_OOT_GERUDO_TRAINING_GROUND, DD_OOT, 0 },
-    { "Ganon",      SCE_OOT_INSIDE_GANON_CASTLE,    DD_OOT, 0 },
+
+static DungeonDef gDungeonDefs[] = {
+    { "Deku",           SCE_OOT_INSIDE_DEKU_TREE,       DD_OOT, 0 },
+    { "Dodongo",        SCE_OOT_DODONGO_CAVERN,         DD_OOT, 0 },
+    { "Jabu",           SCE_OOT_INSIDE_JABU_JABU,       DD_OOT, 0 },
+    { "Forest",         SCE_OOT_TEMPLE_FOREST,          DD_OOT, 5 },
+    { "Fire",           SCE_OOT_TEMPLE_FIRE,            DD_OOT, 8 },
+    { "Water",          SCE_OOT_TEMPLE_WATER,           DD_OOT, 5 },
+    { "Shadow",         SCE_OOT_TEMPLE_SHADOW,          DD_OOT, 5 },
+    { "Spirit",         SCE_OOT_TEMPLE_SPIRIT,          DD_OOT, 5 },
+    { "Well",           SCE_OOT_BOTTOM_OF_THE_WELL,     DD_OOT, 3 },
+    { "Ice",            SCE_OOT_ICE_CAVERN,             DD_OOT, 0 },
+    { "Hideout",        SCE_OOT_THIEVES_HIDEOUT,        DD_OOT, 4 },
+    { "GTG",            SCE_OOT_GERUDO_TRAINING_GROUND, DD_OOT, 9 },
+    { "Ganon",          SCE_OOT_INSIDE_GANON_CASTLE,    DD_OOT, 2 },
+    { "Woodfall",       0,                              DD_MM,  0 },
+    { "Snowhead",       1,                              DD_MM,  0 },
+    { "Great Bay",      2,                              DD_MM,  0 },
+    { "Stone Tower",    3,                              DD_MM,  0 },
 };
 
 static void printChar(GameState_Play* play, char c, float x, float y)
@@ -56,22 +67,53 @@ static void printDigit(GameState_Play* play, int digit, float x, float y)
     printChar(play, '0' + digit, x, y);
 }
 
-static void printDungeonData(GameState_Play* play, int index)
+static void printDungeonData(GameState_Play* play, int base, int index)
 {
-    const DungeonData* data;
+    const DungeonDef* def;
     float y;
 
-    data = gDungeonData + index;
+    def = gDungeonDefs + base + index;
     y = 66.f + 12 * index;
-    printStr(play, data->name, 46.f, y);
+    printStr(play, def->name, 46.f, y);
 }
 
 void comboMenuKeys(GameState_Play* play)
 {
+    static int cursor;
+    static int delay;
+    int change;
+
+    change = 0;
+    if (delay)
+    {
+        delay--;
+    }
+    else
+    {
+        float stickY = play->gs.input[0].current.y / 128.f;
+        if (stickY > 0.5f && cursor > 0)
+        {
+            change = 1;
+            cursor--;
+        }
+
+        if (stickY < -0.5f && cursor < ARRAY_SIZE(gDungeonDefs) - 10)
+        {
+            change = 1;
+            cursor++;
+        }
+
+        if (change)
+        {
+            PlaySound(0x4809);
+            delay = 6;
+        }
+    }
+
     OPEN_DISPS(play->gs.gfx);
     gSPSegment(POLY_XLU_DISP++, 0x06, gCustomKeep);
     comboDrawInit2D(&POLY_XLU_DISP);
     for (int i = 0; i < 10; ++i)
-        printDungeonData(play, i);
+        printDungeonData(play, cursor, i);
     CLOSE_DISPS();
 }
