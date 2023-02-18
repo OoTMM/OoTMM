fe57c788195d0202f8500aa78d48ab6a0ac91f23
Nax <max@bacoux.com>
2023-02-18 15:56:25 +0000

    TS: Basic pipeline system for the logic

--- lib/combo/logic/index.ts
+++ lib/combo/logic/index.ts
@@ -1,6 +1,6 @@
 import { Random } from '../random';
 import { ItemPlacement, solve } from './solve';
-import { createWorld, WorldCheck } from './world';
+import { createWorld, World, WorldCheck } from './world';
 import { spoiler } from './spoiler';
 import { LogicSeedError } from './error';
 import { Options } from '../options';
@@ -8,8 +8,7 @@ import { hints, Hints } from './hints';
 import { alterWorld, configFromSettings } from './settings';
 import { playthrough } from './playthrough';
 import { Monitor } from '../monitor';
-import { EntranceShuffleResult, shuffleEntrances } from './entrance';
-import { EntranceOverrides } from './pathfind';
+import { LogicPassEntrances } from './entrance';
 
 export type LogicResult = {
   items: WorldCheck[];
@@ -19,6 +18,31 @@ export type LogicResult = {
   hash: string;
   entrances: EntranceShuffleResult;
 };
+
+interface LogicPass<Out> {
+  run: () => Out;
+}
+
+type LogicPassConstructor<In, Out> = new (state: In) => LogicPass<Out>;
+
+class LogicPipeline<State> {
+  constructor(private readonly state: State) {
+  }
+
+  apply<Out>(pass: LogicPassConstructor<State, Out>): LogicPipeline<State & Out> {
+    const passInstance = new pass(this.state);
+    const newState = { ...this.state, ...passInstance.run() };
+    return new LogicPipeline(newState);
+  }
+
+  exec(): State {
+    return this.state;
+  }
+};
+
+function pipeline<State>(state: State): LogicPipeline<State> {
+  return new LogicPipeline(state);
+}
 
 const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
@@ -30,20 +54,26 @@ const seedHash = (random: Random): string => {
   return letters.join('');
 };
 
-export const logic = (monitor: Monitor, opts: Options): LogicResult => {
-  const config = configFromSettings(opts.settings);
-  const world = createWorld(opts.settings);
-  alterWorld(world, opts.settings, config);
+const createState = (opts: Options) => {
   const random = new Random();
   random.seed(opts.seed);
-  const entrances = shuffleEntrances(world, random, opts.settings);
+  const world = createWorld(opts.settings);
+  const config = configFromSettings(opts.settings);
+  alterWorld(world, opts.settings, config);
+
+  return { random, world, config, settings: opts.settings };
+};
+
+export const logic = (monitor: Monitor, opts: Options): LogicResult => {
+  const state = pipeline(createState(opts)).apply(LogicPassEntrances).exec();
+  const entrances = shuffleEntrances(state.world, state.random, opts.settings);
 
   let placement: ItemPlacement = {};
   let error: Error | null = null;
   for (let i = 0; i < 100; ++i) {
     try {
       error = null;
-      placement = solve(opts, world, random);
+      placement = solve(opts, state.world, state.random);
       break;
     } catch (e) {
       if (!(e instanceof LogicSeedError)) {
@@ -58,16 +88,16 @@ export const logic = (monitor: Monitor, opts: Options): LogicResult => {
 
   let spheres: string[][] = [];
   if (!opts.settings.noLogic) {
-    spheres = playthrough(opts.settings, random, world, placement);
+    spheres = playthrough(opts.settings, state.random, state.world, placement);
   }
   const items: WorldCheck[] = [];
   for (const loc in placement) {
-    const check = world.checks[loc];
+    const check = state.world.checks[loc];
     items.push({ ...check, item: placement[loc] });
   }
-  const h = hints(monitor, random, opts.settings, world, placement, spheres);
-  const log = spoiler(world, placement, spheres, opts, h, entrances);
-  const hash = seedHash(random);
+  const h = hints(monitor, state.random, opts.settings, state.world, placement, spheres);
+  const log = spoiler(state.world, placement, spheres, opts, h, entrances);
+  const hash = seedHash(state.random);
 
-  return { items, log, hints: h, config, hash, entrances };
+  return { items, log, hints: h, config: state.config, hash, entrances };
 };
