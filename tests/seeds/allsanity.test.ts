df1638114f734b947a79bb970f14a935ed377486
Nax <max@bacoux.com>
2023-02-19 15:30:14 +0000

    TS: Refactor tests

---
+++ tests/seeds/allsanity.test.ts
@@ -0,0 +1,13 @@
+import { makeTestSeed } from '../helper';
+
+test("Can make a seed - allsanity", () => {
+  makeTestSeed("ALLSANITY", {
+    songs: 'anywhere',
+    goldSkulltulaTokens: 'all',
+    housesSkulltulaTokens: 'all',
+    strayFairyShuffle: 'anywhere',
+    townFairyShuffle: 'anywhere',
+    bossKeyShuffle: 'anywhere',
+    smallKeyShuffle: 'anywhere',
+  });
+});
---
+++ tests/seeds/songsanity.test.ts
@@ -0,0 +1,5 @@
+import { makeTestSeed } from "../helper";
+
+test("Can make a seed - songsanity", () => {
+  makeTestSeed("SONGSANITY", { songs: "anywhere" });
+});
