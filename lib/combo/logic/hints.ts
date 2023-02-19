6db057ec04ea1e2be8c5fccd05260c8a1898cf77
Nax <max@bacoux.com>
2023-02-19 02:44:38 +0000

    TS: Fully working refactor

--- lib/combo/logic/hints.ts
+++ lib/combo/logic/hints.ts
@@ -1,45 +1,12 @@
 import { ItemPlacement } from './solve';
 import { World } from './world';
-import { Analysis, findSpheres } from './analysis';
+import { Analysis } from './analysis';
 import { Random, sample, shuffle } from '../random';
-import { addItem, DUNGEON_REWARDS_ORDERED, isDungeonItem, isDungeonReward, isItemMajor, isGoldToken, itemsArray, isKey, isHouseToken, isSmallKey, isGanonBossKey, isRegularBossKey, DUNGEON_REWARDS, isStrayFairy, isToken, isTownStrayFairy, isSong } from './items';
+import { DUNGEON_REWARDS_ORDERED, isDungeonItem, isDungeonReward, isItemMajor, isGoldToken, itemsArray, isKey, isHouseToken, isSmallKey, isGanonBossKey, isRegularBossKey, DUNGEON_REWARDS, isStrayFairy, isToken, isTownStrayFairy, isSong } from './items';
 import { Settings } from '../settings';
 import { Game } from '../config';
 import { Monitor } from '../monitor';
 import { Pathfinder } from './pathfind';
-
-export type HintGossipHero = {
-  type: 'hero',
-  region: string,
-  location: string;
-};
-
-export type HintGossipFoolish = {
-  type: 'foolish',
-  region: string,
-};
-
-export type HintGossipItemExact = {
-  type: 'item-exact',
-  check: string,
-  items: string[],
-};
-
-export type HintGossipItemRegion = {
-  type: 'item-region',
-  region: string,
-  item: string;
-};
-
-export type HintGossip = { game: Game } & (HintGossipHero | HintGossipFoolish | HintGossipItemExact | HintGossipItemRegion);
-
-export type Hints = {
-  dungeonRewards: string[];
-  lightArrow: string;
-  oathToOrder: string;
-  foolish: {[k: string]: number};
-  gossip: {[k: string]: HintGossip};
-};
 
 const FIXED_HINTS_LOCATIONS = [
   'OOT Skulltula House 10 Tokens',
@@ -94,295 +61,44 @@ const HINTS_ITEMS_SOMETIMES = [
   'MM_SECRET_SHRINE_HP',
 ];
 
-const SIMPLE_DEPENDENCIES: {[k: string]: string[]} = {
-  OOT_CHICKEN: [
-    'OOT Lost Woods Gift from Saria',
-    'OOT Zelda\'s Letter',
-    'OOT Zelda\'s Song',
-  ],
-  OOT_MASK_SKULL: [
-    'OOT Deku Theater Sticks Upgrade',
-  ],
-  OOT_MASK_TRUTH: [
-    'OOT Deku Theater Nuts Upgrade'
-  ],
-  OOT_POCKET_CUCCO: [
-    'OOT Kakariko Anju Cojiro'
-  ],
-  OOT_COJIRO: [
-    'OOT Lost Woods Odd Mushroom'
-  ],
-  OOT_ODD_MUSHROOM: [
-    'OOT Kakariko Potion Shop Odd Potion'
-  ],
-  OOT_ODD_POTION: [
-    'OOT Lost Woods Poacher\'s Saw'
-  ],
-  OOT_POACHER_SAW: [
-    'OOT Gerudo Valley Broken Goron Sword'
-  ],
-  OOT_BROKEN_GORON_SWORD: [
-    'OOT Death Mountain Prescription'
-  ],
-  OOT_PRESCRIPTION: [
-    'OOT Zora Domain Eyeball Frog'
-  ],
-  OOT_EYEBALL_FROG: [
-    'OOT Laboratory Eye Drops'
-  ],
-  OOT_EYE_DROPS: [
-    'OOT Death Mountain Claim Check'
-  ],
-  OOT_CLAIM_CHECK: [
-    'OOT Death Mountain Biggoron Sword'
-  ],
-  OOT_WALLET: [
-    'OOT Goron City Medigoron Giant Knife'
-  ],
-  OOT_SONG_SUN: [
-    'OOT Graveyard ReDead Tomb',
-    'OOT Zora River Frogs Game'
-  ],
-  OOT_STONE_EMERALD: [
-    'OOT Hyrule Field Ocarina of Time',
-    'OOT Hyrule Field Song of Time',
-  ],
-  OOT_STONE_RUBY: [
-    'OOT Hyrule Field Ocarina of Time',
-    'OOT Hyrule Field Song of Time',
-  ],
-  OOT_STONE_SAPPHIRE: [
-    'OOT Hyrule Field Ocarina of Time',
-    'OOT Hyrule Field Song of Time',
-  ],
-  OOT_SMALL_KEY_FOREST: [
-    'OOT Forest Temple Map',
-    'OOT Forest Temple Maze',
-    'OOT Forest Temple Garden',
-    'OOT Forest Temple Well',
-    'OOT Forest Temple Poe Key',
-    'OOT Forest Temple Bow',
-    'OOT Forest Temple Compass',
-    'OOT Forest Temple Checkerboard',
-    'OOT Forest Temple Boss Key',
-    'OOT Forest Temple Floormaster',
-    'OOT Forest Temple Antichamber',
-    'OOT Forest Temple GS Entrance',
-    'OOT Forest Temple GS Main',
-    'OOT Forest Temple GS Garden West',
-    'OOT Forest Temple GS Garden East',
-    'OOT Forest Temple GS Antichamber',
-    'OOT Forest Temple Boss Container',
-    'OOT Forest Temple Boss',
-  ],
-  OOT_BOSS_KEY_FOREST: [
-    'OOT Forest Temple Boss Container',
-    'OOT Forest Temple Boss',
-  ],
-  OOT_SMALL_KEY_SPIRIT: [
-    'OOT Spirit Temple Child Climb 1',
-    'OOT Spirit Temple Child Climb 2',
-    'OOT Spirit Temple Statue Base',
-    'OOT Spirit Temple Statue Hands',
-    'OOT Spirit Temple Statue Upper Right',
-    'OOT Spirit Temple Sun Block Room Torches',
-    'OOT Spirit Temple Adult Suns on Wall 1',
-    'OOT Spirit Temple Adult Suns on Wall 2',
-    'OOT Spirit Temple Adult Invisible 1',
-    'OOT Spirit Temple Adult Invisible 2',
-    'OOT Spirit Temple Adult Late Sun on Wall',
-    'OOT Spirit Temple Adult Boss Key Chest',
-    'OOT Spirit Temple Adult Topmost Sun on Wall',
-    'OOT Spirit Temple GS Child Climb',
-    'OOT Spirit Temple GS Iron Knuckle',
-    'OOT Spirit Temple GS Statue',
-    'OOT Spirit Temple Silver Gauntlets',
-    'OOT Spirit Temple Mirror Shield',
-    'OOT Spirit Temple Boss HC',
-    'OOT Spirit Temple Boss',
-  ],
-  OOT_BOSS_KEY_SPIRIT: [
-    'OOT Spirit Temple Boss HC',
-    'OOT Spirit Temple Boss',
-  ],
-  OOT_GS_TOKEN: [
-    'OOT Skulltula House 10 Tokens',
-    'OOT Skulltula House 20 Tokens',
-    'OOT Skulltula House 30 Tokens',
-    'OOT Skulltula House 40 Tokens',
-    'OOT Skulltula House 50 Tokens',
-  ],
-  OOT_SMALL_KEY_GF: [
-    'OOT Gerudo Member Card',
-  ],
-  OOT_SMALL_KEY_GTG: [
-    'OOT Gerudo Training Grounds Stalfos',
-    'OOT Gerudo Training Grounds Near Block',
-    'OOT Gerudo Training Grounds Behind Block Invisible',
-    'OOT Gerudo Training Grounds Behind Block Visible 1',
-    'OOT Gerudo Training Grounds Behind Block Visible 2',
-    'OOT Gerudo Training Grounds Behind Block Visible 3',
-    'OOT Gerudo Training Grounds Eye Statue',
-    'OOT Gerudo Training Grounds Hammer Room Switch',
-    'OOT Gerudo Training Grounds Hammer Room',
-    'OOT Gerudo Training Grounds Lizalfos',
-    'OOT Gerudo Training Water',
-    'OOT Gerudo Training Freestanding Key',
-    'OOT Gerudo Training Maze Upper Cage',
-    'OOT Gerudo Training Maze Upper Fake Ceiling',
-    'OOT Gerudo Training Maze Side Chest 1',
-    'OOT Gerudo Training Maze Side Chest 2',
-    'OOT Gerudo Training Maze Chest 1',
-    'OOT Gerudo Training Maze Chest 2',
-    'OOT Gerudo Training Maze Chest 3',
-    'OOT Gerudo Training Maze Chest 4',
-  ],
-  OOT_SMALL_KEY_GANON: [
-    'OOT Ganon Castle Light Chest Lullaby',
-  ],
-  MM_MASK_CAPTAIN: [
-    'MM Moon Fierce Deity Mask',
-    'MM Beneath The Graveyard Chest',
-    'MM Beneath The Graveyard Song of Storms',
-    'MM Beneath The Graveyard HP',
-    'MM Beneath The Graveyard Dampe Chest',
-    'MM Ocean Spider House Chest HP'
-  ],
-  MM_MASK_ALL_NIGHT: [
-    'MM Moon Fierce Deity Mask',
-    'MM Stock Pot Inn Grandma HP 1',
-    'MM Stock Pot Inn Grandma HP 2'
-  ],
-  MM_MASK_BUNNY: [
-    'MM Moon Fierce Deity Mask',
-    'MM Post Office HP'
-  ],
-  MM_MASK_KEATON: [
-    'MM Moon Fierce Deity Mask',
-    'MM Clock Town Keaton HP'
-  ],
-  MM_MASK_ROMANI: [
-    'MM Moon Fierce Deity Mask',
-    'MM Milk Bar Troupe Leader Mask'
-  ],
-  MM_MASK_TROUPE_LEADER: [
-    'MM Moon Fierce Deity Mask'
-  ],
-  MM_MASK_POSTMAN: [
-    'MM Moon Fierce Deity Mask',
-    'MM Clock Town Post Box'
-  ],
-  MM_MASK_COUPLE: [
-    'MM Moon Fierce Deity Mask',
-    'MM Mayor\'s Office HP'
-  ],
-  MM_MASK_GREAT_FAIRY: [
-    'MM Moon Fierce Deity Mask',
-    'MM Woodfall Great Fairy',
-    'MM Snowhead Great Fairy',
-    'MM Great Bay Great Fairy',
-  ],
-  MM_MASK_DON_GERO: [
-    'MM Moon Fierce Deity Mask',
-    'MM Mountain Village Frog Choir HP'
-  ],
-  MM_MASK_KAMARO: [
-    'MM Moon Fierce Deity Mask',
-    'MM Clock Town Rosa Sisters HP'
-  ],
-  MM_MASK_TRUTH: [
-    'MM Moon Fierce Deity Mask',
-    'MM Doggy Racetrack HP'
-  ],
-  MM_MASK_STONE: [
-    'MM Moon Fierce Deity Mask'
-  ],
-  MM_MASK_BREMEN: [
-    'MM Moon Fierce Deity Mask',
-    'MM Cucco Shack Bunny Mask'
-  ],
-  MM_MASK_KAFEI: [
-    'MM Moon Fierce Deity Mask',
-    'MM Stock Pot Inn Letter to Kafei',
-    'MM Stock Pot Inn Couple\'s Mask',
-    'MM Milk Bar Madame Aroma Bottle'
-  ],
-  MM_DEED_SWAMP: [
-    'MM Stock Pot Inn ??? HP',
-    'MM Goron Village HP',
-    'MM Goron Village Scrub Deed'
-  ],
-  MM_DEED_MOUNTAIN: [
-    'MM Stock Pot Inn ??? HP',
-    'MM Zora Hall Scrub HP',
-    'MM Zora Hall Scrub Deed'
-  ],
-  MM_DEED_OCEAN: [
-    'MM Stock Pot Inn ??? HP',
-    'MM Ikana Valley Scrub Rupee',
-    'MM Ikana Valley Scrub HP'
-  ],
-  MM_ROOM_KEY: [
-    'MM Stock Pot Inn Letter to Kafei',
-    'MM Stock Pot Inn Couple\'s Mask',
-    'MM Stock Pot Inn Guest Room Chest'
-  ],
-  MM_LETTER_TO_KAFEI: [
-    'MM Stock Pot Inn ??? HP',
-    'MM Stock Pot Inn Couple\'s Mask',
-    'MM Curiosity Shop Back Room Pendant of Memories',
-    'MM Curiosity Shop Back Room Owner Reward 1',
-    'MM Curiosity Shop Back Room Owner Reward 2'
-  ],
-  MM_PENDANT_OF_MEMORIES: [
-    'MM Stock Pot Inn Couple\'s Mask'
-  ],
-  MM_LETTER_TO_MAMA: [
-    'MM Stock Pot Inn ??? HP',
-    'MM Milk Bar Madame Aroma Bottle',
-    'MM Clock Town Postman Hat'
-  ],
-  MM_POWDER_KEG: [
-    'MM Romani Ranch Epona Song',
-    'MM Romani Ranch Aliens',
-    'MM Romani Ranch Cremia Escort',
-    'MM Ancient Castle of Ikana Song Emptiness'
-  ],
-  MM_SONG_HEALING: [
-    'MM Termina Field Kamaro Mask',
-    'MM Goron Graveyard Mask',
-    'MM Great Bay Coast Zora Mask',
-    'MM Music Box House Gibdo Mask'
-  ],
-  MM_GS_TOKEN_SWAMP: [
-    'MM Swamp Spider House Mask of Truth',
-  ],
-  MM_GS_TOKEN_OCEAN: [
-    'MM Ocean Spider House Wallet',
-  ],
-  MM_STRAY_FAIRY_TOWN: [
-    'MM Clock Town Great Fairy',
-    'MM Clock Town Great Fairy Alt',
-  ],
-  MM_STRAY_FAIRY_WF: [
-    'MM Woodfall Great Fairy',
-  ],
-  MM_STRAY_FAIRY_SH: [
-    'MM Snowhead Great Fairy',
-  ],
-  MM_STRAY_FAIRY_GB: [
-    'MM Great Bay Great Fairy',
-  ],
-  MM_STRAY_FAIRY_ST: [
-    'MM Ikana Great Fairy',
-  ]
+export type HintGossipHero = {
+  type: 'hero',
+  region: string,
+  location: string;
+};
+
+export type HintGossipFoolish = {
+  type: 'foolish',
+  region: string,
+};
+
+export type HintGossipItemExact = {
+  type: 'item-exact',
+  check: string,
+  items: string[],
+};
+
+export type HintGossipItemRegion = {
+  type: 'item-region',
+  region: string,
+  item: string;
+};
+
+export type HintGossip = { game: Game } & (HintGossipHero | HintGossipFoolish | HintGossipItemExact | HintGossipItemRegion);
+
+export type Hints = {
+  dungeonRewards: string[];
+  lightArrow: string;
+  oathToOrder: string;
+  foolish: {[k: string]: number};
+  gossip: {[k: string]: HintGossip};
 };
 
 export class LogicPassHints {
   private hintedLocations = new Set<string>();
   private gossip: {[k: string]: HintGossip} = {};
   private foolish: {[k: string]: number} = {};
-  private dependencies: typeof SIMPLE_DEPENDENCIES;
+  private woth: Set<string>;
   private pathfinder: Pathfinder;
 
   constructor(
@@ -395,11 +111,8 @@ export class LogicPassHints {
       analysis: Analysis,
     },
   ){
-    this.dependencies = JSON.parse(JSON.stringify(SIMPLE_DEPENDENCIES));
-    if (!state.settings.tricks['OOT_NIGHT_GS']) {
-      delete this.dependencies['OOT_SONG_SUN'];
-    }
     this.pathfinder = new Pathfinder(state.world, state.settings);
+    this.woth = new Set(Array.from(this.state.analysis.required).filter(loc => this.isLocationHintableHero(loc)));
   }
 
   private findItem(item: string) {
@@ -462,28 +175,18 @@ export class LogicPassHints {
     return true;
   }
 
-  private isLocationWayOfTheHero(loc: string) {
+  private isLocationHintableHero(loc: string) {
     const item = this.state.items[loc];
     if (!this.isLocationHintable(loc)) {
+      return false;
+    }
+    if (isKey(item) || isStrayFairy(item) || isToken(item) || isDungeonReward(item)) {
       return false;
     }
     if (isSong(item) && this.state.settings.songs !== 'anywhere') {
       return false;
     }
     return true;
-  }
-
-  private wayOfTheHero() {
-    const woth = new Set<string>();
-    for (const sphere of this.state.analysis.spheres) {
-      for (const loc of sphere) {
-        if (findSpheres(this.state.settings, this.state.world, this.state.items, undefined, new Set([loc])) === null) {
-          woth.add(loc);
-        }
-      }
-    }
-    const locs = Array.from(woth).filter(loc => this.isLocationWayOfTheHero(loc));
-    return new Set(locs);
   }
 
   private findValidGossip(locs: Set<string> | string) {
@@ -498,88 +201,16 @@ export class LogicPassHints {
     return sample(this.state.random, gossips);
   }
 
-  private wayOfTheHeroItems(woth: Set<string>) {
-    const items: {[k: string]: Set<string> } = {};
-    for (const loc of woth) {
-      const item = this.state.items[loc];
-      items[item] ||= new Set<string>();
-      items[item].add(loc);
-    }
-    return items;
-  }
-
   private playthroughLocations() {
     const locations = this.state.analysis.spheres.flat().filter(loc => this.isLocationHintable(loc));
     return shuffle(this.state.random, locations);
   }
 
-  private majorItemFoolish(loc: string, item: string, wothItems: {[k: string]: Set<string>}) {
-    /* TODO: this is fragile */
-    let maximumRequired = -1;
-    switch (item) {
-    case 'OOT_SWORD':
-      maximumRequired = 2;
-      break;
-    case 'OOT_OCARINA':
-    case 'OOT_BOMB_BAG':
-    case 'OOT_BOW':
-    case 'OOT_SLINGSHOT':
-    case 'OOT_WALLET':
-    case 'MM_SWORD':
-    case 'MM_BOW':
-    case 'MM_BOMB_BAG':
-      maximumRequired = 1;
-      break;
-    }
-    if (maximumRequired === -1) {
-      return false;
-    }
-    const woth = wothItems[item];
-    if (!woth) {
-      return false;
-    }
-    if (woth.size < maximumRequired) {
-      return false;
-    }
-    if (woth.has(loc)) {
-      return false;
-    }
-    this.state.monitor.debug("majorItemFoolish - Purged: " + item);
-    return true;
-  }
-
-  private isItemUseless(loc: string) {
-    const locsToCheck = [loc];
-    const locsAdded = new Set<string>(locsToCheck);
-
-    while (locsToCheck.length > 0) {
-      const l = locsToCheck.pop()!;
-      const item = this.state.items[l];
-      if (isItemMajor(item) || isDungeonReward(item) || isKey(item) || isStrayFairy(item) || isToken(item)) {
-        /* May be a progression item - need to check other locations */
-        const dependencies = this.dependencies[item];
-        if (dependencies === undefined) {
-          return false;
-        } else {
-          for (const d of dependencies) {
-            if (!locsAdded.has(d)) {
-              locsAdded.add(d);
-              locsToCheck.push(d);
-            }
-          }
-        }
-      }
-    }
-    this.state.monitor.debug("isItemUseless - Purged: " + this.state.items[loc]);
-    return true;
-  }
-
-  private locationFoolish(loc: string, wothItems: {[k: string]: Set<string>}) {
-    const item = this.state.items[loc];
+  private locationFoolish(loc: string) {
     if (!this.isLocationHintable(loc)) {
       return 0;
     }
-    if ((isItemMajor(item) || isDungeonReward(item) || isKey(item) || isStrayFairy(item)) && !this.majorItemFoolish(loc, item, wothItems) && !this.isItemUseless(loc)) {
+    if (!this.state.analysis.useless.has(loc)) {
       return -1;
     }
     if (this.hintedLocations.has(loc) || this.state.settings.junkLocations.includes(loc)) {
@@ -588,7 +219,7 @@ export class LogicPassHints {
     return 1;
   }
 
-  private foolishRegions(wothItems: {[k: string]: Set<string>}) {
+  private foolishRegions() {
     let regions: {[k:string]: number} = {};
 
     for (const location in this.state.world.checks) {
@@ -597,7 +228,7 @@ export class LogicPassHints {
       if (regions[region] === -1) {
         continue;
       }
-      const value = this.locationFoolish(location, wothItems);
+      const value = this.locationFoolish(location);
       if (value === -1) {
         regions[region] = -1;
       } else {
@@ -685,19 +316,23 @@ export class LogicPassHints {
     return placed;
   }
 
-  private placeGossipHero(woth: Set<string>) {
-    const locs = Array.from(woth).filter(loc => !this.hintedLocations.has(loc));
-    if (locs.length === 0) {
-      return false;
+  private placeGossipHero(count: number) {
+    let placed = 0;
+    const locs = shuffle(this.state.random, Array.from(this.woth).filter(loc => !this.hintedLocations.has(loc)));
+
+    for (;;) {
+      if (placed >= count || locs.length === 0) {
+        break;
+      }
+      const loc = locs.pop()!;
+      const gossip = this.findValidGossip(loc);
+      if (gossip !== null) {
+        this.gossip[gossip] = { game: this.state.world.gossip[gossip].game, type: 'hero', region: this.state.world.regions[loc], location: loc };
+        this.hintedLocations.add(loc);
+        placed++;
+      }
     }
-    const loc = sample(this.state.random, locs);
-    const gossip = this.findValidGossip(loc);
-    if (gossip === null) {
-      return false;
-    }
-    this.gossip[gossip] = { game: this.state.world.gossip[gossip].game, type: 'hero', region: this.state.world.regions[loc], location: loc };
-    this.hintedLocations.add(loc);
-    return true;
+    return placed;
   }
 
   private placeGossipItemRegion(location: string | null) {
@@ -748,9 +383,7 @@ export class LogicPassHints {
     /* Set the always hinted locations */
     FIXED_HINTS_LOCATIONS.forEach(x => this.hintedLocations.add(x));
 
-    const woth = this.wayOfTheHero();
-    const wothItems = this.wayOfTheHeroItems(woth);
-    this.foolish = this.foolishRegions(wothItems);
+    this.foolish = this.foolishRegions();
     let hints = 0;
 
     /* Place always hints */
@@ -777,16 +410,7 @@ export class LogicPassHints {
     hints += this.placeGossipItemRegionSpheres(4);
 
     /* Place way of the hero hints */
-    let wothHints = 0;
-    for (let i = 0; i < 150; ++i) {
-      if (this.placeGossipHero(woth)) {
-        ++wothHints;
-        ++hints;
-        if (wothHints >= 9) {
-          break;
-        }
-      }
-    }
+    hints += this.placeGossipHero(9);
 
     /* Place remaining hints */
     const missingHints = 34 - hints;
