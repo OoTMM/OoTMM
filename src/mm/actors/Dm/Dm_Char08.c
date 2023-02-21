d65c977b8cd724a96edcbb8802fd849f61fa1dbc
Nax <max@bacoux.com>
2023-02-21 02:57:34 +0000

    MM: Fix an issue with the turtle for dungeon ER

--- src/mm/actors/Dm/Dm_Char08.c
+++ src/mm/actors/Dm/Dm_Char08.c
@@ -5,6 +5,8 @@ void DmChar08_WarpToTemple(Actor* this, GameState_Play* play)
     play->nextEntrance = ((SCE_MM_TEMPLE_GREAT_BAY - 3) << 9) | (0 << 4);
     play->transitionType = TRANS_TYPE_NORMAL;
     play->transitionGfx = TRANS_GFX_BLACK;
+
+    gIsEntranceOverride = 1;
 }
 
 PATCH_FUNC(0x80aafa18, DmChar08_WarpToTemple);
