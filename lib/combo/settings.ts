bb87337df38d7d5a84772a2fdf15e249ee4669df
XenoWars <43147270+XenoWars@users.noreply.github.com>
2023-04-09 20:40:48 -0400

    OOT: Add Gerudo Fortress to Region Shuffle (#368)

--- lib/combo/settings.ts
+++ lib/combo/settings.ts
@@ -439,7 +439,7 @@ export const SETTINGS = [{
   default: false
 }, {
   key: 'erOverworld',
-  name: 'Basic Overworld Shuffle',
+  name: 'Shuffle Major Regions',
   category: 'entrances',
   type: 'boolean',
   default: false
