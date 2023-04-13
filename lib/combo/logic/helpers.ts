bf8a20aabf4c53929c97c239334c2cb70d42c7f0
Nax <max@bacoux.com>
2023-04-13 17:18:39 +0000

    Fix MM spawn point outside of ER (for real this time)

--- lib/combo/logic/helpers.ts
+++ lib/combo/logic/helpers.ts
@@ -1,9 +1,9 @@
 import { Settings } from "../settings";
 
 export function isEntranceShuffle(settings: Settings) {
-  if (settings.erBoss)
+  if (settings.erBoss !== 'none')
     return true;
-  if (settings.erDungeons)
+  if (settings.erDungeons !== 'none')
     return true;
   if (settings.erOverworld)
     return true;
