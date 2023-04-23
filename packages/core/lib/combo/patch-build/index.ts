7cc9fda1743c6b18fd3a672a8a788e47c931a1e1
CelestialPanda- <30158363+Celestial-Panda@users.noreply.github.com>
2023-04-23 16:36:45 +0200

    MM: Speeds up MM Dampe Digging

--- packages/core/lib/combo/patch-build/index.ts
+++ packages/core/lib/combo/patch-build/index.ts
@@ -68,6 +68,7 @@ export function buildPatchfile(args: BuildPatchfileIn): Patchfile {
   miscPatches.writeBlastMaskCooldown(args.settings.blastMaskCooldown, file) // Blast Mask Cooldown settings
   miscPatches.writeClockSpeed(args.settings.clockSpeed, file) // Clock Speed modifier
   miscPatches.speedupDogRace(file) // Dog Race RNG mostly removed
+  miscPatches.speedupDampeMM(file)
 
   /* Fierce Deity + Hookshot + Climb Anywhere changes */
   let anywhere: string[] = []
