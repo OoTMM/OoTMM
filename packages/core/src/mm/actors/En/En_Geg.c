e24c9ee64043b1ec8569746c8899965a99b17c4b
Celeste <30158363+Celestial-Panda@users.noreply.github.com>
2023-04-30 21:37:56 +0200

    MM: Various speedups (#398)
    * MM: Scrubs cutscenes no longer lock you in place
    
    * MM: Soaring cutscene can be skipped with a button input
    
    * MM: Speedup Hungry Goron Cutscene

--- packages/core/src/mm/actors/En/En_Geg.c
+++ packages/core/src/mm/actors/En/En_Geg.c
@@ -22,3 +22,60 @@ int EnGeg_GiveItem(Actor* actor, GameState_Play* play, s16 gi, float a, float b)
 }
 
 PATCH_CALL(0x80bb3290, EnGeg_GiveItem);
+
+/**
+ * Add new points for Hungry Goron to roll on
+ **/
+static Vec3f rollTargets[6] = {
+    {
+        .x = -550,
+        .y = 8,
+        .z = 550
+    },
+    {
+        .x = 220,
+        .y = 43,
+        .z = 525
+    },
+    {
+        .x = 960,
+        .y = 90,
+        .z = 525
+    },
+    {
+        .x = 2060,
+        .y = 16,
+        .z = 925
+    },
+    {
+        .x = 2710,
+        .y = -40,
+        .z = 1980
+    },
+    {
+        .x = 3510,
+        .y = -90,
+        .z = 2380
+    }
+};
+
+/**
+ * Hook function used to get the roll target position of the goron.
+ **/
+Vec3f* DonGero_GetRollTarget(s16 index) {
+    if (index < 6) {
+        return &rollTargets[index];
+    }
+    return NULL;
+}
+
+/**
+ * Hook function used to override starting the cutscene.
+ **/
+void DonGero_StartCutscene(s16 index, Actor* actor) {
+    if (index == 0x17) {   
+        u8* address = (u8*)actor;
+        u16* rollingAddress = (u16*)(address + 0x4AA);
+        *rollingAddress = 0x180;
+    } 
+}
