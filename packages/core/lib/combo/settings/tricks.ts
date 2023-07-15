94368a4b6e684ee31a2f7fba2b535447cdb2aada
XenoWars <43147270+XenoWars@users.noreply.github.com>
2023-07-15 12:04:21 -0400

    Add Trick for Adult Spirit as Child
    * Add Spirit Temple trick for Adult Spirit as Child
    
    * Add Child Valley Gate trick

--- packages/core/lib/combo/settings/tricks.ts
+++ packages/core/lib/combo/settings/tricks.ts
@@ -26,6 +26,8 @@ export const TRICKS = {
   OOT_DEAD_HAND_STICKS: "Child Dead Hand without Kokiri Sword",
   OOT_BFA_MUDWALLS: "Break mud walls with blue fire arrows",
   OOT_JABU_BOSS_HOVER: "Access Jabu-Jabu Pre-Boss using Hover Boots",
+  OOT_SPIRIT_CHILD_HOVER: "Access Adult Spirit as Child using Hover Boots",
+  OOT_VALLEY_GATE_HOVER: "Reach Gerudo Fortress as Child using Hover Boots",
   MM_LENS: "Fewer Lens Requirements (MM)",
   MM_PALACE_BEAN_SKIP: "Skip Planting Beans in Deku Palace",
   MM_DARMANI_WALL: "Climb Mountain Village Wall Blind",
