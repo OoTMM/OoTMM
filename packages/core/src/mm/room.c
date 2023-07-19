21ebfbd2ececd00d214201b28ce5bcb4a06eba29
Nax <max@bacoux.com>
2023-07-19 16:51:00 +0200

    Fully working room clear flag system with souls

--- packages/core/src/mm/room.c
+++ packages/core/src/mm/room.c
@@ -8,3 +8,15 @@ static void SpawnRoomActors_Wrapper(GameState_Play* play, int id)
 
 PATCH_CALL(0x8012eb18, SpawnRoomActors_Wrapper);
 PATCH_CALL(0x8012ec30, SpawnRoomActors_Wrapper);
+
+void OnRoomChange(void* arg1, void* arg2)
+{
+    void (*OnRoomChangeOriginal)(void*, void*);
+
+    /* Clear some flags */
+    g.silverRupee = 0;
+    g.roomEnemyLackSoul = 0;
+
+    OnRoomChangeOriginal = (void*)0x8012f90c;
+    OnRoomChangeOriginal(arg1, arg2);
+}
