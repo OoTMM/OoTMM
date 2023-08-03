1352a13d6d58ef2937610b60acf726c655335aeb
Nax <max@bacoux.com>
2023-08-03 23:53:03 +0200

    Working flag texture

--- packages/core/src/oot/actors/Obj/Obj_Tsubo.c
+++ packages/core/src/oot/actors/Obj/Obj_Tsubo.c
@@ -1,8 +1,14 @@
 #include <combo.h>
+#include <combo/custom.h>
+
+static ActorFunc sDraw;
 
 void ObjTsubo_InitWrapper(Actor_ObjTsubo* this, GameState_Play* play)
 {
     ActorFunc init;
+
+    /* Compute funcs */
+    sDraw = actorAddr(AC_OBJ_TSUBO, 0x80a65fbc);
 
     /* Set the extended properties */
     this->extendedRoomId = this->base.room;
@@ -37,4 +43,67 @@ void ObjTsubo_SpawnShuffledDrop(Actor_ObjTsubo* this, GameState_Play* play)
     item->extendedRoomId = this->extendedRoomId;
     item->extendedId = this->extendedId;
     item->extendedGi = GI_OOT_RUPEE_GREEN;
+}
+
+static const Gfx kDrawListNormalTop[] = {
+    gsDPLoadTextureBlock(0x06001000, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListNormalSide[] = {
+    gsDPLoadTextureBlock(0x06000000, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 5, 6, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListNormalDangeonTop[] = {
+    gsDPLoadTextureBlock(0x050118A0, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListNormalDangeonSide[] = {
+    gsDPLoadTextureBlock(0x050108A0, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 5, 6, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListMajorTop[] = {
+    gsDPLoadTextureBlock(0x09000000 | CUSTOM_KEEP_POT_MAJOR_TOP, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListMajorSide[] = {
+    gsDPLoadTextureBlock(0x09000000 | CUSTOM_KEEP_POT_MAJOR_SIDE, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 5, 6, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+void ObjTsubo_DrawWrapper(Actor_ObjTsubo* this, GameState_Play* play)
+{
+    const void* dlistSide;
+    const void* dlistTop;
+
+    /* Checks flag and dangeon_keep */
+    if (!comboXflagsGet(play->sceneId, g.sceneSetupId, this->extendedRoomId, this->extendedId))
+    {
+        dlistSide = kDrawListMajorSide;
+        dlistTop = kDrawListMajorTop;
+    }
+    else if (this->base.variable & 0x0100)
+    {
+        dlistSide = kDrawListNormalSide;
+        dlistTop = kDrawListNormalTop;
+    }
+    else
+    {
+        dlistSide = kDrawListNormalDangeonSide;
+        dlistTop = kDrawListNormalDangeonTop;
+    }
+
+    /* Setup the textures */
+    OPEN_DISPS(play->gs.gfx);
+    gSPSegment(POLY_OPA_DISP++, 0x09, gCustomKeep);
+    gSPSegment(POLY_OPA_DISP++, 0x0a, dlistSide);
+    gSPSegment(POLY_OPA_DISP++, 0x0b, dlistTop);
+    CLOSE_DISPS();
+
+    /* Draw */
+    sDraw(&this->base, play);
 }
