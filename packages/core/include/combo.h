69780b162c986e6101d9a399996bea682fea9353
Nax <max@bacoux.com>
2023-08-05 00:51:20 +0200

    Fix flying pots draw

--- packages/core/include/combo.h
+++ packages/core/include/combo.h
@@ -28,6 +28,7 @@
 #  include <combo/oot/pause_state.h>
 #  include <combo/oot/actors/Item_Etcetera.h>
 #  include <combo/oot/actors/Obj_Tsubo.h>
+#  include <combo/oot/actors/En_Tubo_Trap.h>
 #  include <combo/oot/actors/En_Ossan.h>
 # endif
 
--- packages/core/src/oot/actors/En/En_Tubo_Trap.c
+++ packages/core/src/oot/actors/En/En_Tubo_Trap.c
@@ -1,2 +1,52 @@
 #include <combo.h>
+#include <combo/custom.h>
 
+static const Gfx kDrawListNormalDangeonTop[] = {
+    gsDPLoadTextureBlock(0x050118A0, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListNormalDangeonSide[] = {
+    gsDPLoadTextureBlock(0x050108A0, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 5, 6, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListMajorTop[] = {
+    gsDPLoadTextureBlock(0x09000000 | CUSTOM_KEEP_POT_MAJOR_TOP, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static const Gfx kDrawListMajorSide[] = {
+    gsDPLoadTextureBlock(0x09000000 | CUSTOM_KEEP_POT_MAJOR_SIDE, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, 5, 6, 0, 0),
+    gsSPEndDisplayList(),
+};
+
+static void EnTuboTrap_Draw(Actor_EnTuboTrap* this, GameState_Play* play)
+{
+    const void* dlistSide;
+    const void* dlistTop;
+
+    /* Checks flag and dangeon_keep */
+    if (!comboXflagsGet(&this->xflag))
+    {
+        dlistSide = kDrawListMajorSide;
+        dlistTop = kDrawListMajorTop;
+    }
+    else
+    {
+        dlistSide = kDrawListNormalDangeonSide;
+        dlistTop = kDrawListNormalDangeonTop;
+    }
+
+    /* Setup the textures */
+    OPEN_DISPS(play->gs.gfx);
+    gSPSegment(POLY_OPA_DISP++, 0x09, gCustomKeep);
+    gSPSegment(POLY_OPA_DISP++, 0x0a, dlistSide);
+    gSPSegment(POLY_OPA_DISP++, 0x0b, dlistTop);
+    CLOSE_DISPS();
+
+    /* Draw the pot */
+    DrawSimpleOpa(play, 0x5017870);
+}
+
+PATCH_FUNC(0x80a78684, EnTuboTrap_Draw);
