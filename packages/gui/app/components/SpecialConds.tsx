e7490969921cffb2bfa0d6455777861fbb5d30e0
Alexandre Hamel <ahamel@landr.com>
2023-08-13 15:06:23 -0400

    fix psecial condition page

--- packages/gui/app/components/SpecialConds.tsx
+++ packages/gui/app/components/SpecialConds.tsx
@@ -4,6 +4,7 @@ import { Settings, SPECIAL_CONDS, SPECIAL_CONDS_KEYS } from '@ootmm/core';
 import { useSettings } from '../contexts/GeneratorContext';
 import { Checkbox } from './Checkbox';
 import { InputNumber } from './InputNumber';
+import Group from './Group';
 
 type SpecialCondsPanelProps = {
   cond: string;
@@ -21,24 +22,28 @@ function SpecialCondsPanel({ cond }: SpecialCondsPanelProps) {
 
   return (
     <form onSubmit={e => e.preventDefault()}>
-      <h2>{SPECIAL_CONDS[cond].name}</h2>
-      {Object.keys(SPECIAL_CONDS_KEYS).map(key =>
-        <Checkbox
-          key={key}
-          label={(SPECIAL_CONDS_KEYS as any)[key]}
-          checked={(c as any)[key]}
-          onChange={x => setSettings({ specialConds: { [cond]: { [key]: x } }} as any)}
-        />
-      )}
-      <InputNumber label="Amount" value={c.count} onChange={x => setSettings({ specialConds: { [cond]: { count: x } }} as any)}/>
+      <Group direction="vertical">
+        <h1>{SPECIAL_CONDS[cond].name}</h1>
+        <Group direction="vertical" spacing='4px'> 
+          {Object.keys(SPECIAL_CONDS_KEYS).map(key =>
+            <Checkbox
+              key={key}
+              label={(SPECIAL_CONDS_KEYS as any)[key]}
+              checked={(c as any)[key]}
+              onChange={x => setSettings({ specialConds: { [cond]: { [key]: x } }} as any)}
+            />
+          )}
+         </Group>
+        <InputNumber label="Amount" value={c.count} onChange={x => setSettings({ specialConds: { [cond]: { count: x } }} as any)}/>
+      </Group>
     </form>
   );
 }
 
 export function SpecialConds() {
   return (
-    <div className="three-column-grid">
+    <Group direction="horizontal" spacing='4rem'>
       {Object.keys(SPECIAL_CONDS).map(x => <SpecialCondsPanel key={x} cond={x}/>)}
-    </div>
+    </Group>
   );
 }
