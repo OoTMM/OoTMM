e9b06eeeefe5658c6b3999f0e5edfd2cbac0f9ef
LumineonRL <thebaconator25@aim.com>
2023-08-30 00:58:49 -0400

    Added glitches, macros, & overworld logic

--- packages/core/lib/combo/settings/glitches.ts
+++ packages/core/lib/combo/settings/glitches.ts
@@ -1,6 +1,8 @@
 export const GLITCHES = {
   OOT_EQUIP_SWAP: "Equip Swap (OoT)",
   OOT_OCARINA_ITEMS: "Ocarina Items (OoT)",
+  MM_SAKON_HIDEOUT_CLIP: "Sakon Hideout Clip",
+  MM_FD_ANYWHERE: "Fierce Diety Anywhere (Night 3)",
 }
 
 export type Glitch = keyof typeof GLITCHES;
