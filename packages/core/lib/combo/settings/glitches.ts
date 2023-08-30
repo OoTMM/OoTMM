bf196cdf400a8be58b897e2fb0edb7324e9d8632
LumineonRL <thebaconator25@aim.com>
2023-08-30 01:01:32 -0400

    Fixed spelling of Deity

--- packages/core/lib/combo/settings/glitches.ts
+++ packages/core/lib/combo/settings/glitches.ts
@@ -2,7 +2,7 @@ export const GLITCHES = {
   OOT_EQUIP_SWAP: "Equip Swap (OoT)",
   OOT_OCARINA_ITEMS: "Ocarina Items (OoT)",
   MM_SAKON_HIDEOUT_CLIP: "Sakon Hideout Clip",
-  MM_FD_ANYWHERE: "Fierce Diety Anywhere (Night 3)",
+  MM_FD_ANYWHERE: "Fierce Deity Anywhere (Night 3)",
 }
 
 export type Glitch = keyof typeof GLITCHES;
