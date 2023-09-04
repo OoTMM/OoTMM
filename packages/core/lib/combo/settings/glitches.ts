8af6e12c1e45b12c7369b23c121388fae1a48168
CelestialKitsune_ <30158363+CelestialKitsune@users.noreply.github.com>
2023-09-04 21:39:08 +0200

    Add GUI option for megaflips

--- packages/core/lib/combo/settings/glitches.ts
+++ packages/core/lib/combo/settings/glitches.ts
@@ -1,6 +1,7 @@
 export const GLITCHES = {
   OOT_EQUIP_SWAP: "Equip Swap (OoT)",
   OOT_OCARINA_ITEMS: "Ocarina Items (OoT)",
+  OOT_MEGAFLIP: "Megaflips (OoT)",
 }
 
 export type Glitch = keyof typeof GLITCHES;
