540f8e7fe3b369c59b97d55bcbbae957828ff8e8
Nax <max@bacoux.com>
2023-09-12 19:20:18 +0200

    Disable MM skybox when going to MM (need to make sure this works on ED)

--- packages/core/src/mm/ovl/file_choose.c
+++ packages/core/src/mm/ovl/file_choose.c
@@ -2,9 +2,15 @@
 
 void hook_FileChoose_Update(void* fc)
 {
+    GfxContext* gfx;
+
     /* Save file index */
     *(u16*)((char*)fc + 0x24480) = gComboCtx.saveIndex;
 
     *(u16*)((char*)fc + 0x2448c) = 7;
     *(u16*)((char*)fc + 0x24484) = 2;
+
+    /* Disable video */
+    gfx = *(GfxContext**)fc;
+    *(u32*)gfx->polyOpa.ptr = 0xdf000000;
 }
