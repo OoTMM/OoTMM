093a616279653730fc74a97fd1bdc329962d5ad4
Nax <max@bacoux.com>
2023-09-14 05:53:18 +0200

    Made carried grass respect CSMC

--- packages/core/src/mm/actors/Obj/Obj_Grass_Carry.c
+++ packages/core/src/mm/actors/Obj/Obj_Grass_Carry.c
@@ -1,4 +1,6 @@
 #include <combo.h>
+#include <combo/item.h>
+#include <combo/csmc.h>
 
 void ObjGrass_GetXflag(Xflag* xflag, Actor_ObjGrass_PackBush* bush);
 
@@ -24,3 +26,28 @@ void ObjGrassCarry_SpawnDropsWrapper(Actor_ObjGrassCarry* this)
     ObjGrassCarry_SpawnDrops = actorAddr(AC_OBJ_GRASS_CARRY, 0x809aaf9c);
     ObjGrassCarry_SpawnDrops(&this->base.position, this->drop, gPlay);
 }
+
+static void ObjGrassCarry_Draw(Actor_ObjGrassCarry* this, GameState_Play* play)
+{
+    Xflag xflag;
+    ComboItemOverride o;
+
+    o.gi = 0;
+    if (comboConfig(CFG_MM_SHUFFLE_GRASS))
+    {
+        /* Extract the ID and build the xflag */
+        ObjGrass_GetXflag(&xflag, this->bush);
+
+        /* Check if the reward was already collected */
+        if (!comboXflagsGet(&xflag))
+            comboXflagItemOverride(&o, &xflag, 0);
+    }
+
+    /* Prepare the draw */
+    csmcGrassPreDraw(play, o.gi, CSMC_GRASS_NORMAL, 0, 0);
+
+    /* Draw */
+    DrawSimpleOpa(play, 0x50078a0);
+}
+
+PATCH_FUNC(0x809abb7c, ObjGrassCarry_Draw);
