6f58f14bbef8a6d0ecf64848dd33521fafd54190
CelestialKitsune_ <30158363+CelestialKitsune@users.noreply.github.com>
2023-11-08 23:41:21 +0100

    Add 2 tricks

--- packages/core/lib/combo/settings/tricks.ts
+++ packages/core/lib/combo/settings/tricks.ts
@@ -85,6 +85,8 @@ export const TRICKS = {
   MM_GBT_CENTRAL_GEYSER: "GBT Central Room without Zora using Fire & Ice Arrows",
   MM_BANK_ONE_WALLET: "Bank Rewards Require One Less Wallet",
   MM_BANK_NO_WALLET: "Bank Rewards Require No Wallets",
+  OOT_GANON_CASTLE_ENTRY: "Enter Ganon's Castle with Longshot Anywhere",
+  OOT_SHADOW_TEMPLE_STICKS: "Enter Shadow Temple with Sticks & Fire Arrows",
 };
 
 export type Trick = keyof typeof TRICKS;
