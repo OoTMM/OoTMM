5b4dfe76d234b240dd2cce15c10f3754f44f81eb
CelestialKitsune_ <30158363+CelestialKitsune@users.noreply.github.com>
2023-11-23 03:19:29 +0100

    Add adult trade on dpad up and fix child trade not always showing

--- packages/core/src/common/dpad.c
+++ packages/core/src/common/dpad.c
@@ -184,6 +184,11 @@ static void dpadUseItem(GameState_Play* play, int index, int flags)
             gComboTriggersData.events.weirdEgg = 1;
             return;
         }
+        if (itemId == ITEM_OOT_POCKET_EGG)
+        {
+            gComboTriggersData.events.pocketEgg = 1;
+            return;
+        }
         Player_UseItem = OverlayAddr(0x80834000);
         Player_UseItem(play, GET_LINK(play), itemId);
     }
@@ -209,22 +214,20 @@ void comboDpadUpdate(GameState_Play* play)
 {
     /* Update the items */
     sDpadItems[DPAD_DOWN] = gSave.inventory.items[ITS_OOT_OCARINA];
-
-    if (gSave.age == AGE_CHILD && !comboConfig(CFG_OOT_AGELESS_BOOTS))
+    sDpadItems[DPAD_LEFT] = (gSave.inventory.equipment.boots & EQ_OOT_BOOTS_IRON) ? ITEM_OOT_BOOTS_IRON : ITEM_NONE;
+    sDpadItems[DPAD_RIGHT] = (gSave.inventory.equipment.boots & EQ_OOT_BOOTS_HOVER) ? ITEM_OOT_BOOTS_HOVER : ITEM_NONE;
+    sDpadItems[DPAD_UP] = gSave.inventory.items[ITS_OOT_TRADE_ADULT];
+    if (gSave.age == AGE_CHILD) 
     {
         sDpadItems[DPAD_UP] = gSave.inventory.items[ITS_OOT_TRADE_CHILD];
-        sDpadItems[DPAD_LEFT] = ITEM_NONE;
-        sDpadItems[DPAD_RIGHT] = ITEM_NONE;
-    }
-    else
-    {   
-        sDpadItems[DPAD_UP] = ITEM_NONE;
-        sDpadItems[DPAD_LEFT] = (gSave.inventory.equipment.boots & EQ_OOT_BOOTS_IRON) ? ITEM_OOT_BOOTS_IRON : ITEM_NONE;
-        sDpadItems[DPAD_RIGHT] = (gSave.inventory.equipment.boots & EQ_OOT_BOOTS_HOVER) ? ITEM_OOT_BOOTS_HOVER : ITEM_NONE;
-        if(comboConfig(CFG_OOT_AGELESS_CHILD_TRADE)) {
-            sDpadItems[DPAD_UP] = gSave.inventory.items[ITS_OOT_TRADE_CHILD];
+        if (!comboConfig(CFG_OOT_AGELESS_BOOTS))
+        {
+            sDpadItems[DPAD_LEFT] = ITEM_NONE;
+            sDpadItems[DPAD_RIGHT] = ITEM_NONE;
         }
     }
+    else if(comboConfig(CFG_OOT_AGELESS_CHILD_TRADE))
+        sDpadItems[DPAD_UP] = gSave.inventory.items[ITS_OOT_TRADE_CHILD];
 }
 #endif
 
