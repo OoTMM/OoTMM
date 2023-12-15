cb84e135454daae53ffbb43d379bcf00bfb109ed
Nax <max@bacoux.com>
2023-12-15 18:33:04 +0100

    Fix MM chest game

--- packages/core/src/mm/actors/En/En_Box.c
+++ packages/core/src/mm/actors/En/En_Box.c
@@ -2,35 +2,6 @@
 #include <combo/item.h>
 #include <combo/csmc.h>
 
-static void EnBox_ItemQuery(ComboItemQuery* q, Actor* this, GameState_Play* play, s16 gi)
-{
-    memset(q, 0, sizeof(*q));
-
-    q->gi = gi;
-    if (play->sceneId == SCE_MM_TREASURE_SHOP && gi == -GI_MM_HEART_PIECE)
-    {
-        q->ovType = OV_NPC;
-        q->id = NPC_MM_CHEST_GAME;
-    }
-    else
-    {
-        q->ovType = OV_CHEST;
-        q->sceneId = comboSceneKey(play->sceneId);
-        q->id = this->variable & 0x1f;
-    }
-}
-
-static s16 EnBox_Item(Actor* this, GameState_Play* play, s16 gi)
-{
-    ComboItemQuery q;
-    ComboItemOverride ov;
-
-    EnBox_ItemQuery(&q, this, play, gi);
-    comboItemOverride(&ov, &q);
-
-    return ov.gi;
-}
-
 static s16 convertGi(s16 initial)
 {
     switch (initial)
@@ -57,11 +28,42 @@ static s16 EnBox_GetGI(Actor* this)
     return (s16)(-raw);
 }
 
+static void EnBox_ItemQuery(ComboItemQuery* q, Actor* this, GameState_Play* play)
+{
+    s16 gi;
+    memset(q, 0, sizeof(*q));
+
+    gi = EnBox_GetGI(this);
+    q->gi = gi;
+    if (play->sceneId == SCE_MM_TREASURE_SHOP && gi == -GI_MM_HEART_PIECE)
+    {
+        q->ovType = OV_NPC;
+        q->id = NPC_MM_CHEST_GAME;
+    }
+    else
+    {
+        q->ovType = OV_CHEST;
+        q->sceneId = comboSceneKey(play->sceneId);
+        q->id = this->variable & 0x1f;
+    }
+}
+
+static s16 EnBox_Item(Actor* this, GameState_Play* play)
+{
+    ComboItemQuery q;
+    ComboItemOverride ov;
+
+    EnBox_ItemQuery(&q, this, play);
+    comboItemOverride(&ov, &q);
+
+    return ov.gi;
+}
+
 void EnBox_GiveItemDefaultRange(Actor* actor, GameState_Play* play, s16 gi)
 {
     ComboItemQuery q;
 
-    EnBox_ItemQuery(&q, actor, play, gi);
+    EnBox_ItemQuery(&q, actor, play);
     comboGiveItem(actor, play, &q, 50.f, 10.f);
 }
 
@@ -77,7 +79,7 @@ void EnBox_InitWrapper(Actor* this, GameState_Play* play)
     init(this, play);
 
     /* Resize chest */
-    gi = EnBox_Item(this, play, EnBox_GetGI(this));
+    gi = EnBox_Item(this, play);
     csmcChestInit(this, play, gi);
 }
 
@@ -87,7 +89,7 @@ void EnBox_DrawWrapper(Actor* this, GameState_Play* play)
     s16 gi;
 
     /* Prepare the segments */
-    gi = EnBox_Item(this, play, EnBox_GetGI(this));
+    gi = EnBox_Item(this, play);
     csmcChestPreDraw(this, play, gi);
 
     /* Draw */
