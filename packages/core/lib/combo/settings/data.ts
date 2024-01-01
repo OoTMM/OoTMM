e1b026235b0ce5930df633d148c59b28f46fd6aa
XenoWars <sonicunleashedwr@gmail.com>
2024-01-01 14:09:08 -0500

    Fix typo

--- packages/core/lib/combo/settings/data.ts
+++ packages/core/lib/combo/settings/data.ts
@@ -1395,7 +1395,7 @@ export const SETTINGS = [{
   type: 'boolean',
   description: 'If turned on, it means the boss-containing dungeons and uninverted Stone Tower Temple will be shuffled.',
   default: false,
-  cond: (x: any) => (x.erDungeons !== 'none' && !x.preCompletedDungeons
+  cond: (x: any) => (x.erDungeons !== 'none' && !x.preCompletedDungeons)
 }, {
   key: 'erMinorDungeons',
   name: 'Shuffle OoT Minor Dungeons with Dungeons',
