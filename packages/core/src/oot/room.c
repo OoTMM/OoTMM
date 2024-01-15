f6c76b379bc81270d0c47f8bb247d42b6395c318
Nax <max@bacoux.com>
2024-01-15 04:21:01 +0100

    Re-enable some title screen music fix for music rando

--- packages/core/src/oot/room.c
+++ packages/core/src/oot/room.c
@@ -96,10 +96,7 @@ void ParseSceneRoomHeaders_SoundSettingsWrapper(GameState_Play* play, void* cmd)
 {
     static u32 kMuteCmd[] = { 0x15000000, 0x00000000 };
 
-    /* Disabled as it's suspected to cause ED64 crashes */
-    /*
     if (gComboCtx.valid)
         cmd = kMuteCmd;
-    */
     ParseSceneRoomHeaders_SoundSettings(play, cmd);
 }
