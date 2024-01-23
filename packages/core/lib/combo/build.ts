c1d10293afe25afb42ba30d5eaf964f8adc7f3e5
Nax <max@bacoux.com>
2024-01-23 19:43:04 +0100

    Cleanup

--- packages/core/lib/combo/build.ts
+++ packages/core/lib/combo/build.ts
@@ -1,7 +1,6 @@
 import childProcess from "child_process";
 import fs from 'fs';
 import path from 'path';
-import os from 'os';
 
 import { GAMES } from "./config";
 import { Options } from "./options";
@@ -33,25 +32,7 @@ const cloneDependencies = async () => {
   });
 };
 
-const makeOld = async (opts: Options) => {
-  await cloneDependencies();
-  return new Promise((resolve, reject) => {
-    const args = ['-j', os.availableParallelism().toString()];
-    if (opts.debug) {
-      args.push('DEBUG=1');
-    }
-    const proc = childProcess.spawn('make', args, { stdio: 'inherit' });
-    proc.on('close', (code) => {
-      if (code === 0) {
-        resolve(null);
-      } else {
-        reject(new Error(`make exited with code ${code}`));
-      }
-    });
-  });
-};
-
-const runCommand = async (cmd: string, args: string[]) => {
+async function runCommand(cmd: string, args: string[]) {
   return new Promise((resolve, reject) => {
     const proc = childProcess.spawn(cmd, args, { stdio: 'inherit' });
     proc.on('close', (code) => {
@@ -62,19 +43,26 @@ const runCommand = async (cmd: string, args: string[]) => {
       }
     });
   });
-};
+}
 
-const make = async (opts: Options) => {
+async function make(opts: Options) {
+  /* Clone dependencies */
   await cloneDependencies();
+
+  /* Resolve paths */
   const installDir = path.resolve(__dirname, '..', '..', 'build');
   const buildDir = path.resolve(installDir, 'tree', opts.debug ? 'Debug' : 'Release');
   const sourceDir = path.resolve(__dirname, '..', '..');
+
+  /* Make directories */
   await fs.promises.mkdir(buildDir, { recursive: true });
   await fs.promises.mkdir(installDir, { recursive: true });
+
+  /* Build and install with CMake */
   await runCommand('cmake', ['-B', buildDir, '-S', sourceDir, '-G', 'Ninja', `-DCMAKE_BUILD_TYPE=${opts.debug ? 'Debug' : 'Release'}`]);
   await runCommand('cmake', ['--build', buildDir]);
   await runCommand('cmake', ['--install', buildDir, '--prefix', installDir]);
-};
+}
 
 const getBuildArtifacts = async (root: string): Promise<BuildOutput> => {
   const [oot, mm] = await Promise.all(GAMES.map(async (g) => {
