31bb52aca89e93b1488257ef1fd026170f216e60
Zoey Zolotova <zoey.zolotova@gmail.com>
2024-02-28 09:36:48 +1100

    Auto formatted Custom_Torch2 files with clang. Remove play2 variable.

--- packages/core/src/oot/actors/Custom/Custom_Torch2.c
+++ packages/core/src/oot/actors/Custom/Custom_Torch2.c
@@ -9,15 +9,15 @@
 
 #define FLAGS (1 << 4) // (ACTOR_FLAG_10)
 
-void EnTorch2_Init(Actor_CustomEnTorch2* this, GameState_Play* play);
-void EnTorch2_Destroy(Actor_CustomEnTorch2* this, GameState_Play* play);
-void EnTorch2_Update(Actor_CustomEnTorch2* this, GameState_Play* play);
-void EnTorch2_Draw(Actor_CustomEnTorch2* this, GameState_Play* play2);
+void EnTorch2_Init(Actor_CustomEnTorch2 *this, GameState_Play *play);
+void EnTorch2_Destroy(Actor_CustomEnTorch2 *this, GameState_Play *play);
+void EnTorch2_Update(Actor_CustomEnTorch2 *this, GameState_Play *play);
+void EnTorch2_Draw(Actor_CustomEnTorch2 *this, GameState_Play *play);
 
-void EnTorch2_UpdateIdle(Actor_CustomEnTorch2* this, GameState_Play* play);
-void EnTorch2_UpdateDeath(Actor_CustomEnTorch2* this, GameState_Play* play);
+void EnTorch2_UpdateIdle(Actor_CustomEnTorch2 *this, GameState_Play *play);
+void EnTorch2_UpdateDeath(Actor_CustomEnTorch2 *this, GameState_Play *play);
 
-extern Actor_CustomEnTorch2* gElegyShell;
+extern Actor_CustomEnTorch2 *gElegyShell;
 
 ActorInit En_Torch2_InitVars = {
     AC_CUSTOM_TORCH2,
@@ -42,13 +42,13 @@ static ColliderCylinderInit sCylinderInit = {
     },
     {
         ELEMTYPE_UNK2,
-        { 0x00100000, 0, 0 },
-        { 0xF7CFFFFF, 0, 0 },
+        {0x00100000, 0, 0},
+        {0xF7CFFFFF, 0, 0},
         TOUCH_NONE,
         BUMP_ON | BUMP_HOOKABLE,
         OCELEM_ON,
     },
-    { 20, 60, 0, { 0, 0, 0 } },
+    {20, 60, 0, {0, 0, 0}},
 };
 
 static InitChainEntry sInitChain[] = {
@@ -96,8 +96,7 @@ static Gfx gElegyShellHumanDL[] = {
     gsSP2Triangles(5, 6, 7, 0, 8, 9, 10, 0),
     gsSPDisplayList(0x0C000000),
     gsDPSetTextureLUT(G_TT_NONE),
-    gsDPLoadTextureBlock(sElegyShellHumanEyeAndNoseTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_MIRROR |
-                         G_TX_CLAMP, G_TX_MIRROR | G_TX_CLAMP, 5, 6, G_TX_NOLOD, G_TX_NOLOD),
+    gsDPLoadTextureBlock(sElegyShellHumanEyeAndNoseTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 64, 0, G_TX_MIRROR | G_TX_CLAMP, G_TX_MIRROR | G_TX_CLAMP, 5, 6, G_TX_NOLOD, G_TX_NOLOD),
     gsDPSetPrimColor(0, 0x80, 255, 255, 255, 255),
     gsSP2Triangles(11, 12, 13, 0, 11, 14, 12, 0),
     gsSP2Triangles(11, 15, 14, 0, 15, 16, 14, 0),
@@ -111,8 +110,7 @@ static Gfx gElegyShellHumanDL[] = {
     gsSP2Triangles(12, 27, 28, 0, 13, 12, 28, 0),
     gsSPDisplayList(0x0C000000),
     gsDPSetTextureLUT(G_TT_NONE),
-    gsDPLoadTextureBlock(sElegyShellHumanNostrilsAndSkinTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 8, 0, G_TX_MIRROR |
-                         G_TX_CLAMP, G_TX_MIRROR | G_TX_CLAMP, 4, 3, G_TX_NOLOD, G_TX_NOLOD),
+    gsDPLoadTextureBlock(sElegyShellHumanNostrilsAndSkinTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 8, 0, G_TX_MIRROR | G_TX_CLAMP, G_TX_MIRROR | G_TX_CLAMP, 4, 3, G_TX_NOLOD, G_TX_NOLOD),
     gsDPSetPrimColor(0, 0x80, 255, 255, 255, 255),
     gsSPVertex(&sElegyShellHumanVtx[60], 7, 0),
     gsSP2Triangles(0, 1, 2, 0, 1, 3, 2, 0),
@@ -188,8 +186,7 @@ static Gfx gElegyShellHumanDL[] = {
     gsSP1Triangle(8, 1, 3, 0),
     gsSPDisplayList(0x0C000000),
     gsDPSetTextureLUT(G_TT_NONE),
-    gsDPLoadTextureBlock(sElegyShellHumanNostrilsAndSkinTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 8, 0, G_TX_MIRROR |
-                         G_TX_CLAMP, G_TX_MIRROR | G_TX_CLAMP, 4, 3, G_TX_NOLOD, G_TX_NOLOD),
+    gsDPLoadTextureBlock(sElegyShellHumanNostrilsAndSkinTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 8, 0, G_TX_MIRROR | G_TX_CLAMP, G_TX_MIRROR | G_TX_CLAMP, 4, 3, G_TX_NOLOD, G_TX_NOLOD),
     gsDPSetPrimColor(0, 0x80, 255, 255, 255, 255),
     gsSPVertex(&sElegyShellHumanVtx[222], 32, 0),
     gsSP2Triangles(0, 1, 2, 0, 1, 3, 2, 0),
@@ -247,7 +244,8 @@ static Gfx gElegyShellHumanDL[] = {
     gsSPEndDisplayList(),
 };
 
-void EnTorch2_Init(Actor_CustomEnTorch2* this, GameState_Play* play) {
+void EnTorch2_Init(Actor_CustomEnTorch2 *this, GameState_Play *play)
+{
     Actor_ProcessInitChain(&this->base, sInitChain);
     Collider_InitCylinder(play, &this->collider);
     Collider_SetCylinder(play, &this->collider, &this->base, &sCylinderInit);
@@ -257,17 +255,20 @@ void EnTorch2_Init(Actor_CustomEnTorch2* this, GameState_Play* play) {
     this->framesUntilNextState = 20;
 }
 
-void EnTorch2_Destroy(Actor_CustomEnTorch2* this, GameState_Play* play) {
+void EnTorch2_Destroy(Actor_CustomEnTorch2 *this, GameState_Play *play)
+{
     Collider_DestroyCylinder(play, &this->collider);
 
     gElegyShell = NULL;
 }
 
-void EnTorch2_Update(Actor_CustomEnTorch2* this, GameState_Play* play) {
+void EnTorch2_Update(Actor_CustomEnTorch2 *this, GameState_Play *play)
+{
     u16 targetAlpha;
     u16 remainingFrames;
 
-    if (this->state == TORCH2_STATE_IDLE) {
+    if (this->state == TORCH2_STATE_IDLE)
+    {
         this->base.update = EnTorch2_UpdateIdle;
         return;
     }
@@ -276,28 +277,39 @@ void EnTorch2_Update(Actor_CustomEnTorch2* this, GameState_Play* play) {
     ActorUpdateVelocity(&this->base);
     Actor_SetCollisionCylinder(play, &this->base, 30.0f, 20.0f, 70.0f, UPDBGCHECKINFO_FLAG_0 | UPDBGCHECKINFO_FLAG_2);
 
-    if (this->framesUntilNextState == 0) {
+    if (this->framesUntilNextState == 0)
+    {
         remainingFrames = 0;
-    } else {
+    }
+    else
+    {
         remainingFrames = --this->framesUntilNextState;
     }
 
-    if (remainingFrames == 0) {
-        if (this->state == TORCH2_STATE_INITIALIZED) {
+    if (remainingFrames == 0)
+    {
+        if (this->state == TORCH2_STATE_INITIALIZED)
+        {
             // Spawn in
-            if (this->alpha == 0) {
+            if (this->alpha == 0)
+            {
                 Math_Vec3f_Copy(&this->base.world.pos, &this->base.home.pos);
                 this->base.rot2.y = this->base.home.rot.y;
                 this->state = TORCH2_STATE_FADING_IN;
             }
             targetAlpha = 0;
-        } else if (this->state == TORCH2_STATE_FADING_IN) {
+        }
+        else if (this->state == TORCH2_STATE_FADING_IN)
+        {
             // Stay semitransparent until the player moves away
-            if ((this->base.xzDistanceFromLink > 32.0f) || (fabsf(this->base.yDistanceFromLink) > 70.0f)) {
+            if ((this->base.xzDistanceFromLink > 32.0f) || (fabsf(this->base.yDistanceFromLink) > 70.0f))
+            {
                 this->state = TORCH2_STATE_SOLID;
             }
             targetAlpha = 60;
-        } else {
+        }
+        else
+        {
             // Once the player has moved away, update collision and become opaque
             Collider_UpdateCylinder(&this->base, &this->collider);
             CollisionCheck_SetOC(play, &play->colChkCtx, &this->collider.base);
@@ -307,17 +319,21 @@ void EnTorch2_Update(Actor_CustomEnTorch2* this, GameState_Play* play) {
     }
 }
 
-void EnTorch2_UpdateIdle(Actor_CustomEnTorch2* this, GameState_Play* play) {
-    if (this->state == TORCH2_STATE_DYING) {
+void EnTorch2_UpdateIdle(Actor_CustomEnTorch2 *this, GameState_Play *play)
+{
+    if (this->state == TORCH2_STATE_DYING)
+    {
         // Start death animation
         this->base.update = EnTorch2_UpdateDeath;
         this->base.velocity.y = 0.0f;
     }
 }
 
-void EnTorch2_UpdateDeath(Actor_CustomEnTorch2* this, GameState_Play* play) {
+void EnTorch2_UpdateDeath(Actor_CustomEnTorch2 *this, GameState_Play *play)
+{
     // Fall down and become transparent, then delete once invisible
-    if (Math_StepToS(&this->alpha, 0, 8)) {
+    if (Math_StepToS(&this->alpha, 0, 8))
+    {
         ActorDestroy(&this->base);
         return;
     }
@@ -326,7 +342,8 @@ void EnTorch2_UpdateDeath(Actor_CustomEnTorch2* this, GameState_Play* play) {
     ActorUpdateVelocity(&this->base);
 }
 
-void EnTorch2_Draw(Actor_CustomEnTorch2* this, GameState_Play* play2) {
+void EnTorch2_Draw(Actor_CustomEnTorch2 *this, GameState_Play *play)
+{
     static Gfx renderModeSetNoneDL[] = {
         gsSPEndDisplayList(),
         gsSPEndDisplayList(),
@@ -338,16 +355,17 @@ void EnTorch2_Draw(Actor_CustomEnTorch2* this, GameState_Play* play2) {
         gsSPEndDisplayList(),
     };
 
-    GameState_Play* play = play2;
-
     OPEN_DISPS(play->gs.gfx);
 
-    if (this->alpha == 255) {
+    if (this->alpha == 255)
+    {
         gSPSegment(POLY_OPA_DISP++, 0x08, gCustomKeep);
         gSPSegment(POLY_OPA_DISP++, 0x0C, renderModeSetNoneDL);
         gDPSetEnvColor(POLY_OPA_DISP++, 255, 255, 255, 255);
         Gfx_DrawDListOpa(play, gElegyShellHumanDL);
-    } else {
+    }
+    else
+    {
         gSPSegment(POLY_XLU_DISP++, 0x08, gCustomKeep);
         gSPSegment(POLY_XLU_DISP++, 0x0C, renderModeSetXluSingleCycleDL);
         gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, 255, this->alpha);
