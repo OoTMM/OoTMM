9bf32dfecacd627d554f6ea5cb9d15e4bf329296
Nax <max@bacoux.com>
2024-04-09 01:58:28 +0200

    Add proper exits to all normal grottos

--- packages/core/src/mm/ovl/play.c
+++ packages/core/src/mm/ovl/play.c
@@ -389,6 +389,14 @@ static const GrottoExit kGrottoExits[] = {
     { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_EAST,  0, { 4450, 254, 925 } },
     { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_WEST,  0, { -2782, 48, -1654 } },
     { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_SOUTH, 0, { -1592, -222, 4622 } },
+
+    /* Normal grottos */
+    { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_NORTH, 0, { -2425, -281, -3291 } },
+    { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_EAST,  0, { 3223, 219, 1417 } },
+    { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_SOUTH, 0, { -2317, -221, 3418 } },
+    { ENTR_MM_TERMINA_FIELD_FROM_CLOCK_TOWN_WEST,  0, { -5159, -281, -571 } },
+    { ENTR_MM_DEKU_PALACE_EXTERIOR_FROM_THRONE, 1, { 1190, 0, 1011 } },
+    { ENTR_MM_TWIN_ISLAND_FROM_MOUNTAIN_VILLAGE, 0, { 589, 195, 53 } },
 };
 
 static void applyGrottoExit(u32* entrance, const GrottoExit* ge)
@@ -449,6 +457,8 @@ static u32 entrGrottoExit(GameState_Play* play)
             case 0x1f: return ENTR_MM_GROTTO_EXIT_GENERIC_GRASS;
             }
             UNREACHABLE();
+        case 0x07: return ENTR_MM_GROTTO_EXIT_DODONGO;
+        case 0x09: return ENTR_MM_GROTTO_EXIT_SCRUB;
         case 0x0a:
             switch (gLastScene)
             {
@@ -456,6 +466,10 @@ static u32 entrGrottoExit(GameState_Play* play)
             case SCE_MM_GREAT_BAY_COAST: return ENTR_MM_GROTTO_EXIT_COW_COAST;
             }
             UNREACHABLE();
+        case 0x0b: return ENTR_MM_GROTTO_EXIT_BIO_BABA;
+        case 0x0c: return ENTR_MM_GROTTO_EXIT_BEAN;
+        case 0x0d: return ENTR_MM_GROTTO_EXIT_PEAHAT;
+        case 0x0e: return ENTR_MM_GROTTO_EXIT_HOT_WATER;
         }
     }
 
