de5baf1b5c34b37c6908c79c952da5b011edc234
lkarvec <lkarvec@gmail.com>
2024-04-14 16:51:32 -0500

    move a lot of base logic for tricks

--- packages/gui/app/components/Tricks.tsx
+++ packages/gui/app/components/Tricks.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import { TRICKS } from '@ootmm/core';
-
 import { useSettings } from '../contexts/GeneratorContext';
-import { ArrayList } from './ArrayList';
-import { Group } from './Group';
 import { Text } from './Text';
+import { TrickKey } from '@ootmm/core/lib/combo/settings';
+import SelectorCard from './SelectorCard';
+import TriforceImg from '../assets/blacktriforce.png';
+import MaskImg from '../assets/blackmask.png';
+import { Col, Row } from 'react-bootstrap';
+import Button from './Button';
 
 const NAMES = {
   OOT: 'Ocarina of Time',
-  MM: 'Majora\'s Mask',
+  MM: "Majora's Mask",
 };
 
-export function GameTricks({ game }: { game: keyof typeof NAMES }) {
+export function GameTricks({ game, img }: { game: keyof typeof NAMES; img: typeof TriforceImg }) {
   const [settings, setSettings] = useSettings();
-  const tricks = Object.keys(TRICKS).filter(x => x.startsWith(game));
-  const options = tricks.map(trick => ({ value: trick, label: TRICKS[trick as keyof typeof TRICKS] }));
+  const tricks: TrickKey[] = Object.keys(TRICKS).filter((x) => x.startsWith(game));
+  const options = tricks.map((trick) => ({ value: trick as TrickKey, label: TRICKS[trick].name }));
 
   const add = (trick: string) => {
-    setSettings({ tricks: { add: [trick as keyof typeof TRICKS] } });
+    setSettings({ tricks: { add: [trick as TrickKey] } });
   };
 
   const remove = (trick: string) => {
-    setSettings({ tricks: { remove: [trick as keyof typeof TRICKS] } });
-  }
+    setSettings({ tricks: { remove: [trick as TrickKey] } });
+  };
 
   return (
-    <Group direction='vertical' spacing='xs'>
-      <Text size="jb">{NAMES[game]}</Text>
-      <ArrayList options={options} selected={settings.tricks.filter(x => tricks.includes(x))} add={add} remove={remove}/>
-    </Group>
+    <>
+      <Row>
+        <Col>
+          <Text size="jb">{NAMES[game]}</Text>
+        </Col>
+        <Col lg="auto">
+          <img src={img} height={100} width={100} />
+        </Col>
+      </Row>
+      <SelectorCard
+        options={options}
+        selected={settings.tricks.filter((x) => tricks.includes(x))}
+        add={add}
+        remove={remove}
+        label="Trick"
+      />
+    </>
   );
 }
 
@@ -36,18 +52,21 @@ export function Tricks() {
   const [_, setSettings] = useSettings();
   const clear = () => {
     setSettings({ tricks: { set: [] } });
-  }
+  };
 
   return (
-    <Group direction='vertical' spacing='xxl'>
+    <>
       <Text size="mg">Tricks</Text>
-      <Group direction='vertical' spacing='xl'>
-        <button className="btn-danger" onClick={clear} style={{width: '120px'}}>Remove All</button>
-        <div className="two-column-grid">
-          <GameTricks game="OOT"/>
-          <GameTricks game="MM"/>
-        </div>
-      </Group>
-    </Group>
-  )
+
+      <Button label="Reset All" variant="danger" className="btn-auto" handleClick={clear}></Button>
+      <Row>
+        <Col lg={12} xl={6}>
+          <GameTricks game="OOT" img={TriforceImg} />
+        </Col>
+        <Col>
+          <GameTricks game="MM" img={MaskImg} />
+        </Col>
+      </Row>
+    </>
+  );
 }
