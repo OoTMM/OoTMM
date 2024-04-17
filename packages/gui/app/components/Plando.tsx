c0b9de6bab6f6309484ab134e218b518137dc08c
lkarvec <lkarvec@gmail.com>
2024-04-17 17:58:25 -0500

    add "btn" so bootstrap classes apply properly

--- packages/gui/app/components/Plando.tsx
+++ packages/gui/app/components/Plando.tsx
@@ -14,41 +14,51 @@ export function Plando() {
   const [settings, setSettings] = useSettings();
   const itemPool = useItemPool();
   const locs = Object.keys(locationList(settings)).sort();
-  const locsOptions = locs.map(loc => ({ value: loc, label: loc }));
-  const itemOptions = Object.keys(itemPool).map(item => ({ value: item, label: itemName(item) }));
+  const locsOptions = locs.map((loc) => ({ value: loc, label: loc }));
+  const itemOptions = Object.keys(itemPool).map((item) => ({ value: item, label: itemName(item) }));
 
   const placeItem = () => {
     if (selectedItem && selectedLoc) {
-      setSettings({ plando: { locations: { [selectedLoc]: selectedItem } }});
+      setSettings({ plando: { locations: { [selectedLoc]: selectedItem } } });
     }
   };
 
   const removeItem = (loc: string) => {
     if (loc) {
-      setSettings({ plando: { locations: { [loc]: null } }});
+      setSettings({ plando: { locations: { [loc]: null } } });
     }
   };
 
   const removeAll = () => {
-    setSettings({ plando: { locations: null }});
+    setSettings({ plando: { locations: null } });
   };
 
   return (
-    <Group direction='vertical' spacing='xxl'>
-      <Text size='mg'>Plando</Text>
-      <Group direction='horizontal' spacing='xl'>
-        <Select className='plando-select' options={locsOptions} onChange={(v) => setSelectedLoc(v?.value)}/>
-        <Select className='plando-select' options={itemOptions} onChange={(v) => setSelectedItem(v?.value)}/>
-        <button className="btn-primary" onClick={placeItem}>Add</button>
-        <button className="btn-danger" onClick={removeAll}>Remove All</button>
+    <Group direction="vertical" spacing="xxl">
+      <Text size="mg">Plando</Text>
+      <Group direction="horizontal" spacing="xl">
+        <Select className="plando-select" options={locsOptions} onChange={(v) => setSelectedLoc(v?.value)} />
+        <Select className="plando-select" options={itemOptions} onChange={(v) => setSelectedItem(v?.value)} />
+        <button className="btn btn-primary" onClick={placeItem}>
+          Add
+        </button>
+        <button className="btn-danger" onClick={removeAll}>
+          Remove All
+        </button>
       </Group>
-      <Group direction='vertical' spacing='xs'>
-        {Object.entries(settings.plando.locations || {}).filter(x => x[1]).map(([loc, item]) => (
-          <Group direction='horizontal' key={loc}>
-            <span className="plando-remove" onClick={() => removeItem(loc)}><FontAwesomeIcon icon={faXmark}/></span>
-            <span className="plando-item">{loc}: {itemName(item!)}</span>
-          </Group>
-        ))}
+      <Group direction="vertical" spacing="xs">
+        {Object.entries(settings.plando.locations || {})
+          .filter((x) => x[1])
+          .map(([loc, item]) => (
+            <Group direction="horizontal" key={loc}>
+              <span className="plando-remove" onClick={() => removeItem(loc)}>
+                <FontAwesomeIcon icon={faXmark} />
+              </span>
+              <span className="plando-item">
+                {loc}: {itemName(item!)}
+              </span>
+            </Group>
+          ))}
       </Group>
     </Group>
   );
--- packages/gui/app/components/RomConfig.tsx
+++ packages/gui/app/components/RomConfig.tsx
@@ -10,49 +10,73 @@ import { Text } from './Text';
 
 export function RomConfig() {
   const { romConfig, setFileBuffer, setSeed } = useRomConfig();
-  const [ isPatch, setIsPatch] = useIsPatch();
+  const [isPatch, setIsPatch] = useIsPatch();
   const { error, generate } = useGenerator();
   const [randomSettings, setRandomSettings] = useRandomSettings();
 
   const isRandomSettings = randomSettings.enabled;
 
-  return <Group direction='vertical' spacing=''>
-    <Text size='mg'>OoTMM Web Generator</Text>
-    <Text size='jb'>Version: {process.env.VERSION}</Text>
-    {error && <div className="generator-error">{error}</div>}
-    <form target="_self" onSubmit={(e) => { e.preventDefault(); generate(); }}>
-      <div className='center'>
-      <Group direction='horizontal' spacing='mg'>
-        <FileSelect logo="oot" file='oot' label="Ocarina of Time (1.0, U or J)" accept=".z64, .n64, .v64" onChange={(f) => setFileBuffer('oot', f)}/>
-        <FileSelect logo="mm" file='mm' label="Majora's Mask (U only)" accept=".z64, .n64, .v64" onChange={(f) => setFileBuffer('mm', f)} />
-        {isPatch && <FileSelect file='patch' logo="ootmm" label="OoTMM Patch File" accept=".ootmm" onChange={(f) => setFileBuffer('patch', f)}/>}
-      </Group>
-      </div>
-      {!isPatch && <Checkbox label="Random Settings" checked={isRandomSettings} onChange={x => setRandomSettings({ enabled: x })}/>}
-      <Group direction='vertical' spacing={ isRandomSettings ? '2px' : '16px'}>
-        {isRandomSettings && <>
-          <Checkbox label="Random Settings: MQ" checked={randomSettings.mq} onChange={x => setRandomSettings({ mq: x })}/>
-          <Checkbox label="Random Settings: Entrances" checked={randomSettings.er} onChange={x => setRandomSettings({ er: x })}/>
-        </>}
-        {!isPatch && !isRandomSettings && <>
-          <Group direction='vertical'>
-            <PresetSelector/>
-            <SettingsImportExport/>
+  return (
+    <Group direction="vertical" spacing="">
+      <Text size="mg">OoTMM Web Generator</Text>
+      <Text size="jb">Version: {process.env.VERSION}</Text>
+      {error && <div className="generator-error">{error}</div>}
+      <form
+        target="_self"
+        onSubmit={(e) => {
+          e.preventDefault();
+          generate();
+        }}
+      >
+        <div className="center">
+          <Group direction="horizontal" spacing="mg">
+            <FileSelect
+              logo="oot"
+              file="oot"
+              label="Ocarina of Time (1.0, U or J)"
+              accept=".z64, .n64, .v64"
+              onChange={(f) => setFileBuffer('oot', f)}
+            />
+            <FileSelect
+              logo="mm"
+              file="mm"
+              label="Majora's Mask (U only)"
+              accept=".z64, .n64, .v64"
+              onChange={(f) => setFileBuffer('mm', f)}
+            />
+            {isPatch && (
+              <FileSelect file="patch" logo="ootmm" label="OoTMM Patch File" accept=".ootmm" onChange={(f) => setFileBuffer('patch', f)} />
+            )}
           </Group>
-        </>}
-        {!isRandomSettings && <Checkbox label="Use a patch file" checked={isPatch} onChange={setIsPatch}/>}
-        {!isPatch && !isRandomSettings && <label>
-          Seed (leave blank to auto-generate)
-          <input
-            type="text"
-            value={romConfig.seed}
-            onChange={(e) => setSeed(e.target.value)}
-          />
-        </label>}
-        <button className="btn-primary" type="submit" style={{width: '105px'}}>
-          Generate
-        </button>
-      </Group>
-    </form>
-  </Group>
+        </div>
+        {!isPatch && <Checkbox label="Random Settings" checked={isRandomSettings} onChange={(x) => setRandomSettings({ enabled: x })} />}
+        <Group direction="vertical" spacing={isRandomSettings ? '2px' : '16px'}>
+          {isRandomSettings && (
+            <>
+              <Checkbox label="Random Settings: MQ" checked={randomSettings.mq} onChange={(x) => setRandomSettings({ mq: x })} />
+              <Checkbox label="Random Settings: Entrances" checked={randomSettings.er} onChange={(x) => setRandomSettings({ er: x })} />
+            </>
+          )}
+          {!isPatch && !isRandomSettings && (
+            <>
+              <Group direction="vertical">
+                <PresetSelector />
+                <SettingsImportExport />
+              </Group>
+            </>
+          )}
+          {!isRandomSettings && <Checkbox label="Use a patch file" checked={isPatch} onChange={setIsPatch} />}
+          {!isPatch && !isRandomSettings && (
+            <label>
+              Seed (leave blank to auto-generate)
+              <input type="text" value={romConfig.seed} onChange={(e) => setSeed(e.target.value)} />
+            </label>
+          )}
+          <button className="btn btn-primary" type="submit" style={{ width: '105px' }}>
+            Generate
+          </button>
+        </Group>
+      </form>
+    </Group>
+  );
 }
