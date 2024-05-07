9a66d5a30f6c36c44bcf1328cae30b3d90ddd5fe
CelestalKitsune <30158363+CelestialKitsune@users.noreply.github.com>
2024-05-07 02:31:44 +0200

    Fix boat cruise not allowing to disembark

--- packages/core/src/mm/actors/Bg/Bg_Ingate.c
+++ packages/core/src/mm/actors/Bg/Bg_Ingate.c
@@ -1,6 +1,6 @@
 #include <combo.h>
 
-int BgIngate_EndArchery(Actor* this, GameState_Play* play) {
+int BgIngate_EndArchery(ActorBgIngate* this, GameState_Play* play) {
     int finished = (this->flags & 2) == 2;
     return finished || (((gSave.eventInf[3] & 0x20) != 0) && (gSaveContext.minigameScore >= 20));
 }
