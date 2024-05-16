c3ad4506b6c8f63c72c83d96487c6f85ccc80f43
Nax <max@bacoux.com>
2024-05-16 21:23:04 +0200

    Rosa sisters cutscene skip

--- packages/core/src/mm/actors/En/En_Rz.c
+++ packages/core/src/mm/actors/En/En_Rz.c
@@ -8,3 +8,10 @@ void EnRz_GiveItem(Actor* this, GameState_Play* play, s16 gi, float a, float b)
 
 PATCH_CALL(0x80bfc2d8, EnRz_GiveItem);
 PATCH_CALL(0x80bfc344, EnRz_GiveItem);
+
+int EnRz_DanceCutscene(Actor* this, GameState_Play* play)
+{
+    PlayerDisplayTextBox(play, 0x2925, this);
+    Message_Close(play);
+    return 1;
+}
