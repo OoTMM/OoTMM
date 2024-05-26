e84859c24f46fb39c40e95f18b61b07648741a05
Nax <max@bacoux.com>
2024-05-26 14:32:43 +0200

    Remove debug log

--- packages/core/lib/combo/logic/solve.ts
+++ packages/core/lib/combo/logic/solve.ts
@@ -621,7 +621,6 @@ export class LogicPassSolver {
         } else {
           locs = new Map(this.locations.map(x => [x, 1] as const));
         }
-        console.log(locs);
         locs = new Map(Array.from(locs.entries()).filter(x => locationData(x[0]).world === worldId && !this.state.items.has(x[0])));
         const medianSphere = Array.from(locs.values()).sort((a, b) => a - b)[Math.floor(locs.size / 2)];
         const underMedian = Array.from(locs.keys()).filter(x => locs.get(x)! < medianSphere);
