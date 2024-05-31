8b116f0b538d2265c0464c1ddae1ea0004803dba
Nax <max@bacoux.com>
2024-05-31 11:41:02 +0200

    Add Lon Lon Ranch

--- packages/core/src/oot/actors/En/En_Wonder_Item.c
+++ packages/core/src/oot/actors/En/En_Wonder_Item.c
@@ -50,6 +50,13 @@ static void EnWonderItem_Alias(Actor_EnWonderItem* this)
             xflag->id += 2;
         }
         break;
+    case SCE_OOT_LON_LON_RANCH:
+        if (xflag->setupId == 3)
+        {
+            xflag->setupId = 2;
+            xflag->id += 16;
+        }
+        break;
     }
 }
 
