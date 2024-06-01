d6d72b17d90fa9d71f18c8b36246b53878c24e44
Nax <max@bacoux.com>
2024-06-01 15:58:05 +0200

    WIP

---
+++ packages/tools/src/zovl.c
@@ -0,0 +1,20 @@
+#include <elf.h>
+#include <stdint.h>
+
+typedef struct
+{
+    char*       data;
+    uint32_t    size;
+}
+StateSection;
+
+typedef struct
+{
+    char*
+}
+State;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
