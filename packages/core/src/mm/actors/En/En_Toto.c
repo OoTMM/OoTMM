b33b3784d4e6c72e79eb8cbbaf0c874dba90da6c
Nax <max@bacoux.com>
2024-06-04 18:18:40 +0200

    WIP

--- packages/core/src/mm/actors/En/En_Toto.c
+++ packages/core/src/mm/actors/En/En_Toto.c
@@ -1,10 +1,30 @@
 #include <combo.h>
 #include <combo/player.h>
+#include <combo/item.h>
+
+void EnToto_UpdateWrapper(Actor* this, GameState_Play* play)
+{
+    int shouldGiveItem;
+    ActorFunc EnToto_Update;
+
+    shouldGiveItem = 0;
+
+    if (shouldGiveItem)
+    {
+
+    }
+    else
+    {
+        EnToto_Update = actorAddr(AC_EN_TOTO, 0x80ba4e2c);
+        EnToto_Update(this, play);
+    }
+}
 
 void EnToto_SpawnItemGiver(Actor* this, GameState_Play* play, s16 gi, float a, float b)
 {
-    comboSpawnItemGiver(play, NPC_MM_MASK_TROUPE_LEADER);
-    this->parent = &GET_LINK(play)->base;
+    comboGiveItemNpc(this, play, gi, NPC_MM_MASK_TROUPE_LEADER, a, b);
+    //comboSpawnItemGiver(play, NPC_MM_MASK_TROUPE_LEADER);
+    //this->parent = &GET_LINK(play)->base;
 }
 
 PATCH_CALL(0x80ba4e10, EnToto_SpawnItemGiver);
