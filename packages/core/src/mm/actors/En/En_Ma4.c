e1799228dc25cbcae7d5cf7d8f8cf51c41764496
Nax <max@bacoux.com>
2024-06-04 16:22:02 +0200

    Cleaner Epona Song hack

--- packages/core/src/mm/actors/En/En_Ma4.c
+++ packages/core/src/mm/actors/En/En_Ma4.c
@@ -1,4 +1,8 @@
 #include <combo.h>
+#include <combo/player.h>
+#include <combo/item.h>
+
+#define SET_HANDLER(a, h) do { *(void**)(((char*)(a)) + 0x33c) = (h); } while (0)
 
 void EnMa4_DisplayTextBoxAfterGame(GameState_Play* play, s16 messageId, Actor* actor)
 {
@@ -7,3 +11,30 @@ void EnMa4_DisplayTextBoxAfterGame(GameState_Play* play, s16 messageId, Actor* a
 }
 
 PATCH_CALL(0x80abee64, EnMa4_DisplayTextBoxAfterGame);
+
+static void EnMa4_HandleLearnSongEpona(Actor* this, GameState_Play* play)
+{
+    Actor_Player* link;
+    void* handler;
+
+    link = GET_LINK(play);
+    if (link->state & PLAYER_ACTOR_STATE_GET_ITEM)
+        return;
+
+    if (Actor_HasParent(this))
+    {
+        this->parent = NULL;
+        gMmExtraFlags.songEpona = 1;
+    }
+
+    if (gMmExtraFlags.songEpona)
+    {
+        handler = actorAddr(AC_EN_MA4, 0x80abe560);
+        SET_HANDLER(this, handler);
+        return;
+    }
+
+    comboGiveItemNpc(this, play, GI_MM_SONG_EPONA, NPC_MM_SONG_EPONA, 10000.f, 5000.f);
+}
+
+PATCH_FUNC(0x80abf4a8, EnMa4_HandleLearnSongEpona);
