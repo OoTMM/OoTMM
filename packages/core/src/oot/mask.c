ec29ed3583abc0360e80398b3c5df9822be62596
Nax <max@bacoux.com>
2024-06-04 23:25:39 +0200

    Better masks hook

---
+++ packages/core/src/oot/mask.c
@@ -0,0 +1,17 @@
+#include <combo.h>
+#include <combo/mask.h>
+#include <combo/player.h>
+
+u16 MaskReaction_GetTextId(GameState_Play* play, int slot)
+{
+    Actor_Player* link;
+
+    link = GET_LINK(play);
+
+    if (link->mask == MASK_KEATON && slot == MASK_REACTION_SET_HYRULIAN_GUARD)
+        return 0x712d;
+
+    return 0;
+}
+
+PATCH_FUNC(0x800597c0, MaskReaction_GetTextId);
