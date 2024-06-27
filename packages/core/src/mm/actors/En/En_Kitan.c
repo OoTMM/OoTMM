f2d3bec8f16193097ef8dbd904c7ba28622a8ac7
CelestalKitsune <30158363+CelestialKitsune@users.noreply.github.com>
2024-06-27 20:48:46 +0200

    Fix vanilla Keaton Quiz music bug

--- packages/core/src/mm/actors/En/En_Kitan.c
+++ packages/core/src/mm/actors/En/En_Kitan.c
@@ -5,6 +5,10 @@
 void EnKitan_GiveItem(Actor* this, GameState_Play* play, s16 gi, float a, float b)
 {
     comboGiveItemNpc(this, play, gi, NPC_MM_KEATON_HEART_PIECE, a, b);
+    
+    // Stop Keaton Quiz Music after giving the item, with a fade out of 128 frames  
+    AudioSeq_QueueSeqCmd((1 << 28) | 0xFF | ((u8)(1) << 24) | ((u8)(128) << 16));
+    // These casts are unnecessary besides specifying the type in case we reuse or expand this into a macro
 }
 
 PATCH_CALL(0x80c096ec, EnKitan_GiveItem);
