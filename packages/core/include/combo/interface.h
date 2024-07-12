b21defa9c245d2fb06440d1c5f0262e5068ca18d
CelestalKitsune <30158363+CelestialKitsune@users.noreply.github.com>
2024-07-12 20:12:24 +0200

    Ovl: Add En_Test4

--- packages/core/include/combo/interface.h
+++ packages/core/include/combo/interface.h
@@ -7,6 +7,7 @@ void Interface_Update(GameState_Play* play);
 
 #if defined(GAME_MM)
 void Interface_NewDay(GameState_Play* play, int day);
+void Interface_SetHudVisibility(u16 hudVisibility);
 #endif
 
 #endif
