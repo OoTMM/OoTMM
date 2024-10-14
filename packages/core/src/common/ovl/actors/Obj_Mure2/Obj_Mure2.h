dfeea3c44388466c981e100925edba91d1c2d519
Nax <max@bacoux.com>
2024-10-14 23:13:04 +0200

    Merge headers

--- packages/core/src/common/ovl/actors/Obj_Mure2/Obj_Mure2.h
+++ packages/core/src/common/ovl/actors/Obj_Mure2/Obj_Mure2.h
@@ -7,18 +7,6 @@ typedef struct Actor_ObjMure2 Actor_ObjMure2;
 
 typedef void (*Actor_ObjMure2Func)(Actor_ObjMure2*, GameState_Play*);
 
-#if defined(GAME_OOT)
-struct Actor_ObjMure2
-{
-    Actor               actor;
-    Actor_ObjMure2Func  actionFunc;
-    Actor*              actorSpawnPtrList[12];
-    u16                 currentActorNum;
-    float               unk_184;
-};
-#endif
-
-#if defined(GAME_MM)
 #define OBJ_MURE2_CHILD_MAX                     12
 #define OBJ_MURE2_GET_CHILD_TYPE(thisx)         ((thisx)->params & 3)
 #define OBJ_MURE2_GET_CHILD_UPPER_PARAMS(thisx) ((((thisx)->params) >> 8) & 0x1F)
@@ -29,8 +17,7 @@ struct Actor_ObjMure2
     Actor_ObjMure2Func  actionFunc;
     Actor*              actors[OBJ_MURE2_CHILD_MAX];
     u16                 spawnFlags;
-    f32                 rangeMultiplier;
+    float               rangeMultiplier;
 };
-#endif
 
 #endif
