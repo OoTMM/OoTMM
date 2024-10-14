f36b512a5f718207e4a5fabc636ee25f889313e8
Nax <max@bacoux.com>
2024-10-14 23:03:54 +0200

    Import MM Obj_Mure2 OVL

---
+++ packages/core/src/mm/ovl/actors/Obj_Mure2/Obj_Mure2.h
@@ -0,0 +1,23 @@
+#ifndef OVL_OBJ_MURE2_H
+#define OVL_OBJ_MURE2_H
+
+#include <combo.h>
+
+typedef struct Actor_ObjMure2 Actor_ObjMure2;
+
+typedef void (*Actor_ObjMure2Func)(Actor_ObjMure2*, GameState_Play*);
+
+#define OBJ_MURE2_CHILD_MAX                     12
+#define OBJ_MURE2_GET_CHILD_TYPE(thisx)         ((thisx)->params & 3)
+#define OBJ_MURE2_GET_CHILD_UPPER_PARAMS(thisx) ((((thisx)->params) >> 8) & 0x1F)
+
+struct Actor_ObjMure2
+{
+    Actor               actor;
+    Actor_ObjMure2Func  actionFunc;
+    Actor*              actors[OBJ_MURE2_CHILD_MAX];
+    u16                 spawnFlags;
+    f32                 rangeMultiplier;
+};
+
+#endif
