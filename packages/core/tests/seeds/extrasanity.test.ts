fc394c9f176c1fb41b24ed4259acb0f4e424d75a
Nax <max@bacoux.com>
2024-10-31 10:34:06 +0100

    Add even more tests

--- packages/core/tests/seeds/extrasanity.test.ts
+++ packages/core/tests/seeds/extrasanity.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from 'vitest';
 
 import { makeTestSeed } from '../helper';
 
-test("Can make a seed - allsanity", async () => {
-  const res = await makeTestSeed("ALLSANITY", {
+test("Can make a seed - Extrasanity", async () => {
+  const res = await makeTestSeed("Extrasanity", {
     songs: 'anywhere',
     goldSkulltulaTokens: 'all',
     housesSkulltulaTokens: 'all',
@@ -25,10 +25,30 @@ test("Can make a seed - allsanity", async () => {
     fairyFountainFairyShuffleOot: true,
     fairyFountainFairyShuffleMm: true,
     scrubShuffleOot: true,
+    scrubShuffleMm: true,
     childWallets: true,
     colossalWallets: true,
     tingleShuffle: 'anywhere',
     owlShuffle: 'anywhere',
+    shufflePotsOot: 'all',
+    shufflePotsMm: 'all',
+    shuffleGrassOot: 'all',
+    shuffleGrassMm: 'all',
+    shuffleTFGrassMm: true,
+    shuffleBarrelsMm: 'all',
+    shuffleCratesOot: 'all',
+    shuffleCratesMm: 'all',
+    shuffleFreeRupeesOot: 'all',
+    shuffleFreeRupeesMm: 'all',
+    shuffleFreeHeartsOot: 'all',
+    shuffleFreeHeartsMm: true,
+    shuffleHivesOot: true,
+    shuffleHivesMm: true,
+    shuffleButterfliesOot: true,
+    shuffleButterfliesMm: true,
+    shuffleWonderItemsOot: 'all',
+    shuffleWonderItemsMm: true,
+    fairySpotShuffleOot: true,
   });
   expect(res).toBeDefined();
 });
