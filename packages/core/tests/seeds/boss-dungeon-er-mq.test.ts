0a65531f0f833735eb4ad83315095e7900e5c688
Nax <max@bacoux.com>
2024-10-31 10:23:10 +0100

    Add more seed tests

---
+++ packages/core/tests/seeds/boss-dungeon-er-mq.test.ts
@@ -0,0 +1,31 @@
+import { test, expect } from 'vitest';
+
+import { makeTestSeed } from '../helper';
+
+
+test("Can make a seed - MQ Boss/Dungeon ER", async () => {
+  const res = await makeTestSeed("BOSS ER", {
+    songs: 'anywhere',
+    erBoss: 'full',
+    erDungeons: 'full',
+    erMajorDungeons: true,
+    erMinorDungeons: true,
+    erSpiderHouses: true,
+    erIkanaCastle: true,
+    dungeon: {
+      DT: 'mq',
+      DC: 'mq',
+      JJ: 'mq',
+      Forest: 'mq',
+      Fire: 'mq',
+      Water: 'mq',
+      Spirit: 'mq',
+      Shadow: 'mq',
+      BotW: 'mq',
+      IC: 'mq',
+      GTG: 'mq',
+      Ganon: 'mq',
+    }
+  });
+  expect(res).toBeDefined();
+});
---
+++ packages/core/tests/seeds/boss-dungeon-er.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from 'vitest';
+
+import { makeTestSeed } from '../helper';
+
+
+test("Can make a seed - Boss/Dungeon ER", async () => {
+  const res = await makeTestSeed("BOSS ER", {
+    songs: 'anywhere',
+    erBoss: 'full',
+    erDungeons: 'full',
+    erMajorDungeons: true,
+    erMinorDungeons: true,
+    erSpiderHouses: true,
+    erIkanaCastle: true,
+  });
+  expect(res).toBeDefined();
+});
