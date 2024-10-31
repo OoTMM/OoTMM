9b9392a5b547d184d666d5aed75a72f90a89d64e
Nax <max@bacoux.com>
2024-10-31 10:10:03 +0100

    WIP aliasing, missing Kokiri Forest

--- packages/core/src/common/ovl/actors/Obj_Mure2/Obj_Mure2.c
+++ packages/core/src/common/ovl/actors/Obj_Mure2/Obj_Mure2.c
@@ -225,6 +225,40 @@ static InitChainEntry sInitChain[] = {
 void ObjMure2_AliasGrass(Xflag* xf)
 {
 #if defined(GAME_OOT)
+    switch (xf->sceneId)
+    {
+    case SCE_OOT_HYRULE_FIELD:
+        switch (xf->setupId)
+        {
+        case 1: xf->id += 2; break;
+        case 2: xf->id += 8; break;
+        }
+        xf->setupId = 0;
+        break;
+    case SCE_OOT_GRAVEYARD:
+        switch (xf->setupId)
+        {
+        case 1: xf->id -= 4; break;
+        case 2: xf->id -= 13; break;
+        case 3: xf->id -= 13; break;
+        }
+        xf->setupId = 0;
+        break;
+    case SCE_OOT_ZORA_RIVER:
+        if (xf->setupId == 2)
+        {
+            xf->id += 28;
+            xf->setupId = 0;
+        }
+        break;
+    case SCE_OOT_LAKE_HYLIA:
+        if (xf->setupId == 2)
+        {
+            xf->setupId = 0;
+            xf->id -= 4;
+        }
+        break;
+    }
 #endif
 
 #if defined(GAME_MM)
