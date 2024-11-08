c10650327eca20dfbfa191951c6678a6ac8b08d7
Nax <max@bacoux.com>
2024-11-08 21:44:39 +0100

    Fully working

--- packages/core/src/mm/ovl_patch/play.c
+++ packages/core/src/mm/ovl_patch/play.c
@@ -730,15 +730,6 @@ void Play_TransitionDone(PlayState* play)
         break;
     }
 
-    /* Game switch */
-    if (entrance == ENTR_MM_CLOCK_TOWER_FROM_CLOCK_TOWN)
-    {
-        if (!Config_Flag(CFG_ONLY_MM))
-            entrance = ENTR_OOT_MARKET_FROM_MASK_SHOP | MASK_FOREIGN_ENTRANCE;
-        else
-            entrance = ENTR_MM_CLOCK_TOWN_FROM_CLOCK_TOWER;
-    }
-
     /* Handle grotto exits */
     if (entrance == ENTR_MM_INTERNAL_EXIT_GROTTO)
     {
