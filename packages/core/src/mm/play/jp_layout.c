deaf3086a9eaeaf390d1e5298533a5447313365c
Nax <max@bacoux.com>
2024-11-10 02:43:23 +0100

    More WIP

--- packages/core/src/mm/play/jp_layout.c
+++ packages/core/src/mm/play/jp_layout.c
@@ -21,21 +21,16 @@ static void ComboPlay_SpawnLayoutDependantRupee(PlayState* play, float x, float 
 
 void ComboPlay_JpLayout(PlayState* play)
 {
-    if (play->sceneId != SCE_MM_DEKU_PALACE)
+    if (!Config_Flag(CFG_MM_JP_LAYOUT) || play->sceneId != SCE_MM_DEKU_PALACE)
         return;
 
     switch (play->roomCtx.curRoom.num)
     {
     case 0x01:
-        /* Right side */
-        Actor_Spawn(&play->actorCtx, play, ACTOR_OBJ_SYOKUDAI, 352, 160, 655, 0x5, 0x7f, 0x2a, 0x207f);
-        Actor_Spawn(&play->actorCtx, play, ACTOR_OBJ_SYOKUDAI, 450, 80, 655, 0x5, 0x7f, 0x2a, 0x207f);
-        Actor_Spawn(&play->actorCtx, play, ACTOR_OBJ_SYOKUDAI, 352, 160, 655, 0x2, 0x7f, 0x55, 0x287f);
-        Actor_Spawn(&play->actorCtx, play, ACTOR_OBJ_SYOKUDAI, 450, 80, 655, 0x2, 0x7f, 0x55, 0x287f);
-
+        /* Top grotto */
         Actor_Spawn(&play->actorCtx, play, ACTOR_DOOR_ANA, 354, 160, 712, 0x7, 0xf, 0x7f, 0x305);
-        Actor_Spawn(&play->actorCtx, play, ACTOR_DOOR_ANA, 451, 80, 726, 0x7, 0x10, 0x7f, 0x306);
 
+        /* Rupees */
         ComboPlay_SpawnLayoutDependantRupee(play, 406, 80, 876, 0x1101, 0x0a);
         ComboPlay_SpawnLayoutDependantRupee(play, 439, 80, 825, 0x1201, 0x0b);
         break;
