55a386a22865eee3f91b7d05f67a00764b083c50
Nax <max@bacoux.com>
2024-11-15 16:36:12 +0100

    Add stub manifest

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -10,6 +10,9 @@ mkdir -p $TMP_DIR
 # Copy data
 cp -r packages/gui/dist $TMP_DIR/generator
 
+# TODO: This can be changed later
+echo '{"generator":{"inject":"iframe"}}' > $TMP_DIR/manifest.json
+
 # Create a ZIP
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR
