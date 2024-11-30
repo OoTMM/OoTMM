2114a10c4967d08a8fd59295928af81a6cc87ead
Nax <4265415+Nax@users.noreply.github.com>
2024-11-30 01:08:06 +0100

    Add boss metadata

--- packages/core/lib/combo/logic/entrance.ts
+++ packages/core/lib/combo/logic/entrance.ts
@@ -107,6 +107,27 @@ export const DUNGEON_ENTRANCES: readonly Entrance[] = [
   'MM_MOON',
 ];
 
+type BossMetadata = {
+  readonly event: string;
+  readonly dungeon: string;
+  readonly eventClear?: string;
+}
+
+export const BOSS_EVENTS: {[k in Entrance]?: BossMetadata} = {
+  OOT_BOSS_DEKU_TREE: { event: 'OOT_BOSS_GOHMA', dungeon: 'DT' },
+  OOT_BOSS_DODONGO_CAVERN: { event: 'OOT_BOSS_KING_DODONGO', dungeon: 'DC' },
+  OOT_BOSS_JABU_JABU: { event: 'OOT_BOSS_BARINADE', dungeon: 'JJ' },
+  OOT_BOSS_TEMPLE_FOREST: { event: 'OOT_BOSS_PHANTOM_GANON', dungeon: 'Forest' },
+  OOT_BOSS_TEMPLE_FIRE: { event: 'OOT_BOSS_VOLVAGIA', dungeon: 'Fire' },
+  OOT_BOSS_TEMPLE_WATER: { event: 'OOT_BOSS_MORPHA', dungeon: 'Water', eventClear: 'OOT_CLEAR_STATE_LAKE' },
+  OOT_BOSS_TEMPLE_SHADOW: { event: 'OOT_BOSS_BONGO_BONGO', dungeon: 'Shadow' },
+  OOT_BOSS_TEMPLE_SPIRIT: { event: 'OOT_BOSS_TWINROVA', dungeon: 'Spirit' },
+  MM_BOSS_TEMPLE_WOODFALL: { event: 'MM_BOSS_ODOLWA', dungeon: 'WF', eventClear: 'MM_CLEAR_STATE_WOODFALL' },
+  MM_BOSS_TEMPLE_SNOWHEAD: { event: 'MM_BOSS_GOHT', dungeon: 'SH', eventClear: 'MM_CLEAR_STATE_SNOWHEAD' },
+  MM_BOSS_TEMPLE_GREAT_BAY: { event: 'MM_BOSS_GYORG', dungeon: 'GB', eventClear: 'MM_CLEAR_STATE_GREAT_BAY' },
+  MM_BOSS_TEMPLE_STONE_TOWER: { event: 'MM_BOSS_TWINMOLD', dungeon: 'IST', eventClear: 'MM_CLEAR_STATE_IKANA' },
+}
+
 class WorldShuffler {
   private world: World;
   private worldChanged: boolean;
