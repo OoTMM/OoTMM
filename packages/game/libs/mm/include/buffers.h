025f752d5d7253cfd8dfd26677db2878a4265412
Nax <4265415+Nax@users.noreply.github.com>
2024-12-11 06:35:24 +0100

    Loader can load MM too

--- packages/game/libs/mm/include/buffers.h
+++ packages/game/libs/mm/include/buffers.h
@@ -42,25 +42,4 @@ extern u16 gFramebufferHiRes0[HIRES_BUFFER_HEIGHT][HIRES_BUFFER_WIDTH];
 
 extern BufferHigh gHiBuffer;
 
-#ifndef FRAMEBUFFERS_START_ADDR
-/**
- * The `framebuffers` segment is located at a fixed location in RAM and has a
- * fixed size.
- * Those framebuffers are placed at the end of the RAM space.
- * This address is calculated by doing `0x80800000 - (size of framebuffers)`,
- * where 0x80800000 is the end of the Expansion Pak address range.
- * In the vanilla game this value expands to `0x80780000`.
- *
- * Since the start of the `framebuffers` segment is the end of the not-fixed
- * available RAM, then the `system_heap` covers all the remaining RAM that is
- * not used by the non-relocatable code/data (i.e. `boot`, `code`, and other
- * buffers) up to the start of the `framebuffers` segmemt.
- * @see `Main`
- */
-#define FRAMEBUFFERS_START_ADDR (PHYS_TO_K0(0x800000) - sizeof(BufferHigh))
-
-static_assert(FRAMEBUFFERS_START_ADDR == 0x80780000, "The expected address of gHiBuffer shifted. Please update said address in buffers.h and in the spec file.");
-#endif
-
-
 #endif
