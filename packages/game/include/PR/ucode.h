09ea98db1b9e9bc85ecd8a4a918d34f03ba9098f
Nax <4265415+Nax@users.noreply.github.com>
2024-12-12 05:49:01 +0100

    Use MM ucode for both games

--- packages/game/include/PR/ucode.h
+++ packages/game/include/PR/ucode.h
@@ -24,8 +24,8 @@ extern u64 gspF3DZEX2_NoN_PosLight_fifoDataStart[];
 extern u64 gspF3DZEX2_NoN_PosLight_fifoDataEnd[];
 
 /* OoT stuff */
-extern u64 gspS2DEX2d_fifoTextStart[], gspS2DEX2d_fifoTextEnd[];
-extern u64 gspS2DEX2d_fifoDataStart[], gspS2DEX2d_fifoDataEnd[];
+extern u64 gspS2DEX2_fifoTextStart[], gspS2DEX2_fifoTextEnd[];
+extern u64 gspS2DEX2_fifoDataStart[], gspS2DEX2_fifoDataEnd[];
 extern u64 njpgdspMainTextStart[], njpgdspMainTextEnd[];
 extern u64 njpgdspMainDataStart[], njpgdspMainDataEnd[];
 extern u64 cic6105TextStart[], cic6105TextEnd[];
