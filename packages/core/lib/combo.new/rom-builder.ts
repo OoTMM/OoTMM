9691faf1d7510cc9fac30117511a8aa5b756ec49
Nax <4265415+Nax@users.noreply.github.com>
2024-12-26 19:38:33 +0100

    Can generate a basic combined ROM out of the decomp (doesn't load yet)

---
+++ packages/core/lib/combo.new/rom-builder.ts
@@ -0,0 +1,102 @@
+import { bufReadU32BE, bufWriteU32BE } from '../combo/util/buffer';
+import { RandoFile, RandoFileSystem } from './file-system';
+
+const rol = (v: number, b: number) => (((v << b) | (v >>> (32 - b))) & 0xffffffff) >>> 0;
+
+function checksum(rom: Uint8Array) {
+  const seed = 0xdf26f436;
+  let t1 = seed;
+  let t2 = seed;
+  let t3 = seed;
+  let t4 = seed;
+  let t5 = seed;
+  let t6 = seed;
+
+  for (let i = 0; i < 0x100000 / 4; ++i) {
+    const offset = 0x1000 + i * 4;
+    const d = bufReadU32BE(rom, offset);
+    if ((((t6 + d) & 0xffffffff) >>> 0) < t6) {
+      t4 = ((t4 + 1) & 0xffffffff) >>> 0;
+    }
+    t6 = ((t6 + d) & 0xffffffff) >>> 0;
+    t3 = ((t3 ^ d) & 0xffffffff) >>> 0;
+    const r = rol(d, ((d & 0x1f) >>> 0));
+    t5 = ((t5 + r) & 0xffffffff) >>> 0;
+    if (t2 > d) {
+      t2 = (t2 ^ r) >>> 0;
+    } else {
+      t2 = (t2 ^ t6 ^ d) >>> 0;
+    }
+    const offset2 = 0x750 + ((i * 4) & 0xff);
+    const x = bufReadU32BE(rom, offset2);
+    t1 = ((t1 + ((x ^ d) >>> 0)) & 0xffffffff) >>> 0;
+  }
+
+  return [(t6 ^ t4 ^ t3) >>> 0, (t5 ^ t2 ^ t1) >>> 0];
+}
+
+class RomBuilder {
+  private rom: Uint8Array;
+  private fileSystem: RandoFileSystem;
+  private injected: Set<RandoFile>;
+  private pos: number;
+
+  constructor(fileSystem: RandoFileSystem) {
+    this.rom = new Uint8Array(128 * 1024 * 1024);
+    this.fileSystem = fileSystem;
+    this.injected = new Set();
+    this.pos = 0;
+  }
+
+  private fixChecksum() {
+    const [c1, c2] = checksum(this.rom);
+    bufWriteU32BE(this.rom, 0x10, c1);
+    bufWriteU32BE(this.rom, 0x14, c2);
+  }
+
+  private inject(file: RandoFile | string) {
+    /* Resolve the file */
+    if (typeof file === 'string') {
+      const f = this.fileSystem.findFile(file);
+      if (!f) {
+        throw new Error(`File not found: ${file}`);
+      }
+      file = f;
+    }
+
+    /* Check if already injected */
+    if (this.injected.has(file)) {
+      return;
+    }
+
+    /* Inject the file */
+    const data = file.data!;
+    this.rom.set(data, this.pos);
+    this.injected.add(file);
+
+    /* Update the position */
+    this.pos += (data.length + 15) & 0xfffffff0;
+  }
+
+  build() {
+    /* We need to inject the makerom and loader first */
+    this.inject('makerom');
+    this.inject('loader');
+
+    /* Inject all files */
+    for (const file of this.fileSystem.files) {
+      this.inject(file);
+    }
+
+    /* Fix the checksum */
+    this.fixChecksum();
+
+    return this.rom;
+  }
+}
+
+export function buildRom(fileSystem: RandoFileSystem) {
+  const builder = new RomBuilder(fileSystem);
+  return builder.build();
+}
+
--- packages/core/lib/new-gen.ts
+++ packages/core/lib/new-gen.ts
@@ -1,8 +1,14 @@
+import { promises as fs } from 'fs';
 import { makeRandoFileSystem } from './combo.new/file-system-builder';
+import { buildRom } from './combo.new/rom-builder';
+import path from 'path';
 
 async function run() {
-  const fs = await makeRandoFileSystem();
-  console.log(fs);
+  const fileSystem = await makeRandoFileSystem();
+  const rom = buildRom(fileSystem);
+  const dir = path.resolve(__dirname, '..', 'out');
+  await fs.mkdir(dir, { recursive: true });
+  await fs.writeFile(path.resolve(dir, 'OoTMM.z64'), rom);
 }
 
 run().catch((err) => {
