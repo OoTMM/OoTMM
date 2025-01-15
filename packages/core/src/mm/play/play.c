fd9f46e9efd590941bf89e9fbf526fdca390cf70
Nax <4265415+Nax@users.noreply.github.com>
2025-01-15 21:59:21 +0100

    Correctly implemented in OoT

--- packages/core/src/mm/play/play.c
+++ packages/core/src/mm/play/play.c
@@ -549,6 +549,7 @@ void hookPlay_Init(PlayState* play)
     g.prevRoom = -1;
     gIsEntranceOverride = 0;
     g.decoysCount = 0;
+    g.bronzeScaleSolidGround = 0;
     isEndOfGame = 0;
     gActorCustomTriggers = NULL;
     gMultiMarkChests = 0;
--- packages/core/src/oot/actors/Player.c
+++ packages/core/src/oot/actors/Player.c
@@ -263,6 +263,44 @@ static void updateKokiriSwordLength(void)
     }
 }
 
+static Vec3f sBronzeScaleGroundPos;
+static s16 sBronzeScaleGroundRot;
+static u8 sBronzeScaleGroundRoom;
+
+static void Player_HandleBronzeScale(Player* this, PlayState* play)
+{
+    if (gSharedCustomSave.bronzeScaleOot)
+        return;
+
+    if (!(this->stateFlags1 & (PLAYER_ACTOR_STATE_WATER | PLAYER_ACTOR_STATE_CLIMB | PLAYER_ACTOR_STATE_CLIMB2 | PLAYER_ACTOR_STATE_GET_ITEM | PLAYER_ACTOR_STATE_JUMPING) || (this->actor.bgCheckFlags & BGCHECKFLAG_WATER) || !(this->actor.bgCheckFlags & BGCHECKFLAG_GROUND)))
+    {
+        sBronzeScaleGroundPos = this->actor.world.pos;
+        sBronzeScaleGroundRot = this->actor.world.rot.y;
+        sBronzeScaleGroundRoom = play->roomCtx.curRoom.num;
+        g.bronzeScaleSolidGround = 1;
+        return;
+    }
+
+    if (this->currentBoots == 2)
+        return;
+
+    if (!(this->stateFlags1 & PLAYER_ACTOR_STATE_WATER))
+        return;
+
+    if (!g.bronzeScaleSolidGround)
+        return;
+
+    if (play->transitionTrigger)
+        return;
+
+    /* Player is in deep water with no bronze scale */
+    PlaySound(NA_SE_EV_WATER_CONVECTION);
+    Play_SetRespawnData(play, RESPAWN_MODE_DOWN, gSave.entrance, sBronzeScaleGroundRoom, 0xdff, &sBronzeScaleGroundPos, sBronzeScaleGroundRot);
+    gSaveContext.respawnFlag = 1;
+    gSaveContext.nextCutscene = 0;
+    comboTransition(gPlay, gSave.entrance);
+}
+
 void Player_UpdateWrapper(Player* this, PlayState* play)
 {
     updateKokiriSwordLength();
@@ -276,6 +314,7 @@ void Player_UpdateWrapper(Player* this, PlayState* play)
 
     ArrowCycle_Handle(this, play);
     Player_Update(this, play);
+    Player_HandleBronzeScale(this, play);
     Ocarina_HandleCustomSongs(this, play);
     Dpad_Update(play);
     Dpad_Use(play, DPF_EQUIP);
