e31ffb61f2547a4827dd643689fe452cab2a58f6
Nax <4265415+Nax@users.noreply.github.com>
2025-01-17 19:22:17 +0100

    Rename moon crash behavior reset -> last save

--- packages/core/lib/combo/settings/data.ts
+++ packages/core/lib/combo/settings/data.ts
@@ -900,7 +900,7 @@ export const SETTINGS = [{
   type: 'enum',
   description: 'Change the behavior of moon crashing',
   values: [
-    { value: 'reset', name:  'Reset',  description: 'Moon Crash will restore the last save. No progress will be kept.' },
+    { value: 'reset', name:  'Last Save',  description: 'Moon Crash will restore the last save. No progress will be kept.' },
     { value: 'cycle', name:  'New Cycle',  description: 'Moon Crash will initiate a new cycle, keeping progress. Saving is enabled on the Clock Tower Roof.' },
   ],
   cond: hasMM,
