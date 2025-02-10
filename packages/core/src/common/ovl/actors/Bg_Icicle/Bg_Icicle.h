0a1082e81e9eeca6d868defdc4c532823422a2bb
CelestalKitsune <30158363+CelestialKitsune@users.noreply.github.com>
2025-02-10 15:49:24 +0100

    Icicles merged overlay

---
+++ packages/core/src/common/ovl/actors/Bg_Icicle/Bg_Icicle.h
@@ -0,0 +1,31 @@
+#ifndef COMBO_BG_ICICLE_H
+#define COMBO_BG_ICICLE_H
+
+#include <combo.h>
+
+struct Actor_BgIcicle;
+
+typedef void (*BgIceTuraraActionFunc)(struct Actor_BgIcicle*, struct PlayState*);
+
+typedef enum BgIceTuraraType {
+    TURARA_STALAGMITE,
+    ICICLE_STALAGMITE_RANDOM_DROP = TURARA_STALAGMITE,
+    TURARA_STALACTITE,
+    ICICLE_STALACTITE = TURARA_STALACTITE,
+    TURARA_STALACTITE_REGROW,
+    ICICLE_STALACTITE_REGROW = TURARA_STALACTITE_REGROW,
+    ICICLE_STALAGMITE_FIXED_DROP,
+} BgIceTuraraType;
+
+typedef struct Actor_BgIcicle {
+    DynaPolyActor dyna;
+    BgIceTuraraActionFunc actionFunc;
+#if defined(GAME_MM)
+    u8 unk_160;
+    u8 unk_161;
+#endif
+    s16 shiverTimer;
+    ColliderCylinder collider;
+} Actor_BgIcicle;
+
+#endif
