53e79b6a1bb44d17f488b8dcefe0ffb7dea4474d
Eric <ericbrown10001@gmail.com>
2025-02-21 21:00:35 -0800

    Remove constraint for notes

--- packages/core/lib/combo/settings/data.ts
+++ packages/core/lib/combo/settings/data.ts
@@ -8,13 +8,6 @@ const hasOoTMM = (x: any) => x.games === 'ootmm';
 const hasOoT = (x: any) => hasGame(x, 'oot');
 const hasMM = (x: any) => hasGame(x, 'mm');
 
-const notesRequirements = (s: any) => {
-  return (
-      (s.shuffleGrassMm === 'all' || s.shuffleGrassOot === 'all')
-      || (s.shufflePotsMm === 'all' || s.shufflePotsOot === 'all')
-  );
-};
-
 const SETTING_PRICE = {
   type: 'enum',
   values: [
@@ -213,8 +206,7 @@ export const SETTINGS = [{
   values: [
     { value: 'songLocations', name: 'Song Locations', description: 'Only locations that contain songs in the vanilla games will have songs.' },
     { value: 'anywhere', name: 'Anywhere', description: 'Songs can be placed anywhere.' },
-    { value: 'notes', name: 'Shuffle Notes', description: 'Song Notes are shuffled and must be found to unlock song. ' +
-          'Due to the number of notes among the songs, this option requires at least PotSanity or GrassSanity to be enabled. ', cond: notesRequirements },
+    { value: 'notes', name: 'Shuffle Notes', description: 'Song Notes are shuffled and must be found to unlock song.'},
   ],
   default: 'songLocations'
 }, {
