bd4e43412889b54d2aeebbd793039362c06ba5d6
Nax <4265415+Nax@users.noreply.github.com>
2025-05-09 19:12:23 +0200

    Some more minor CI fixes

--- packages/web/deploy.sh
+++ packages/web/deploy.sh
@@ -5,5 +5,10 @@ function die() {
   exit 1
 }
 
+if [ ! -z "$DEPLOY_STABLE" ]; then
+  echo "No need to deploy web on stable"
+  exit 0
+fi
+
 # Deploy to Netlify
 ../../node_modules/.bin/netlify deploy -d "$PWD/dist" --site $NETLIFY_SITE_ID_WEB --prod || die "Failed to deploy"
