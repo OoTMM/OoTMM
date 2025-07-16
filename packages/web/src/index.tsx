5f6ace9037cf68893bc833bc97c5774123de3819
Nax <4265415+Nax@users.noreply.github.com>
2025-07-16 20:56:46 +0200

    Rework web a little bit

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,11 +1,11 @@
 import { render } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
 
-import { Header } from './components/Header';
-import { Home } from './pages/Home';
-import { Generator } from './pages/Generator';
-import { GeneratorVersions } from './pages/GeneratorVersions';
-import { NotFound } from './pages/NotFound';
+import { Header } from './app/components/Header';
+import { Home } from './app/pages/Home';
+import { Generator } from './app/pages/Generator';
+import { GeneratorVersions } from './app/pages/GeneratorVersions';
+import { NotFound } from './app/pages/NotFound';
 import Faq from './pages/Faq.mdx';
 import Multiplayer from './pages/Multiplayer.mdx';
 
--- packages/web/vite-env.d.ts
+++ packages/web/vite-env.d.ts
@@ -1,1 +1,6 @@
 declare const __STATIC_URL__: string;
+
+declare module "*.mdx" {
+  let MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
+}
