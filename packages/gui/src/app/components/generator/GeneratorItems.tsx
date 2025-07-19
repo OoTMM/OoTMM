bb1f9513cf9cf2b6c5ee8f5af47a0a9f50b30fa7
Nax <4265415+Nax@users.noreply.github.com>
2025-07-19 22:44:49 +0200

    Icons everywhere

--- packages/gui/src/app/components/generator/GeneratorItems.tsx
+++ packages/gui/src/app/components/generator/GeneratorItems.tsx
@@ -1,3 +1,5 @@
+import { LuPackage, LuUsers, LuPuzzle, LuClock, LuTrendingUp } from 'react-icons/lu';
+
 import { useSetting } from '@/app/contexts/SettingsContext';
 import { TabView, TabViewRoute } from '../nav';
 import { StartingItems } from '../StartingItems';
@@ -6,11 +8,11 @@ import { SettingsEditor } from '../settings';
 export function GeneratorItems() {
   const games = useSetting('games');
   const routes: TabViewRoute[] = [
-    { name: 'Progressive', component: () => <SettingsEditor category='items.progressive'/> },
-    { name: 'Shared', component: () => <SettingsEditor category='items.shared'/>, disabled: games !== 'ootmm' },
-    { name: 'Extensions', component: () => <SettingsEditor category='items.extensions'/> },
-    { name: 'Ageless', component: () => <SettingsEditor category='items.ageless'/>, disabled: games === 'mm' },
-    { name: 'Starting Items', component: StartingItems }
+    { name: 'Progressive', icon: LuTrendingUp, component: () => <SettingsEditor category='items.progressive'/> },
+    { name: 'Shared', icon: LuUsers, component: () => <SettingsEditor category='items.shared'/>, disabled: games !== 'ootmm' },
+    { name: 'Extensions', icon: LuPuzzle, component: () => <SettingsEditor category='items.extensions'/> },
+    { name: 'Ageless', icon: LuClock, component: () => <SettingsEditor category='items.ageless'/>, disabled: games === 'mm' },
+    { name: 'Starting Items', icon: LuPackage, component: StartingItems }
   ];
 
   return <TabView routes={routes}/>;
