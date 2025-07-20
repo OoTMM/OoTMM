574535d30a8f48a491a73b99fb2670d0b20385a4
Nax <4265415+Nax@users.noreply.github.com>
2025-07-21 00:59:07 +0200

    Minor graphical file select fix

--- packages/gui/src/app/components/ui/FileSelect.tsx
+++ packages/gui/src/app/components/ui/FileSelect.tsx
@@ -26,8 +26,8 @@ export const FileSelect = ({ imageSrc, accept, onInput, file, ...props }: FileSe
   return (
     <div className="ux-bg ux-border w-[200px] h-[200px] min-w-[200px] min-h-[200px] relative">
       <input type="file" accept={accept} className="opacity-0 w-full h-full cursor-pointer" onInput={handleInput} {...props}/>
-      <img src={imageSrc} className={clsx("absolute z-10 w-full h-full object-contain scale-75 inset-0 pointer-events-none", file ? '' : 'grayscale' )}/>
-      <span className="absolute bottom-1 left-0 w-full text-center select-none pointer-events-none overflow-ellipsis">{file ? file.name : "No file selected"}</span>
+      <img src={imageSrc} className={clsx("absolute w-full h-full object-contain scale-75 inset-0 pointer-events-none", file ? '' : 'grayscale' )}/>
+      <span className="absolute bottom-1 left-0 w-full text-center select-none pointer-events-none overflow-hidden overflow-ellipsis">{file ? file.name : "No file selected"}</span>
       {file && <span className="absolute top-1 right-1 cursor-pointer hover:text-gray-500" onClick={handleReset}><FaXmark/></span>}
     </div>
   );
