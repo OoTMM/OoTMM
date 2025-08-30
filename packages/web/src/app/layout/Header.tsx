de5d56a4f89b7919532c76b53335e4c6b5b55b8f
Nax <4265415+Nax@users.noreply.github.com>
2025-08-30 17:10:29 +0200

    Minor website improvements, add wiki link

--- packages/web/src/app/layout/Header.tsx
+++ packages/web/src/app/layout/Header.tsx
@@ -3,15 +3,16 @@ import { ThemeSwitch } from '../theme/ThemeSwitch';
 
 export function Header() {
 	return (
-		<header className="flex items-center h-[80px] bg-black text-white">
+		<header className="flex items-center h-16 bg-gradient-to-r from-blue-900 to-purple-900 text-white">
       <a href="/" className="flex items-center nav-link">
-        <img className="rounded-[100%] h-[80px] object-contain p-3 mr-5" alt="OoTMM Logo" src={logo}/>
+        <img className="rounded-2xl h-16 object-contain p-1 ml-3 mr-1" alt="OoTMM Logo" src={logo}/>
         <h1 className="text-4xl font-bold">OoTMM</h1>
       </a>
       <nav className="flex text-xl items-center gap-8 ml-10">
         <a className="nav-link" href="/faq">FAQ</a>
-        <a className="nav-link" target="_blank" href="https://discord.gg/4QdtPBP6wf">Discord</a>
         <a className="nav-link" href="/multiplayer">Multiplayer Guide</a>
+        <a className="nav-link" target="_blank" href="https://discord.gg/4QdtPBP6wf">Discord</a>
+        <a className="nav-link" target="_blank" href="https://ootmm.wiki.gg">Wiki</a>
       </nav>
       <div className="flex-grow"></div>
       <div className="mr-8">
