caa58b8b495f33350ec5792562958784a21d91fc
Nax <4265415+Nax@users.noreply.github.com>
2025-10-12 20:55:37 +0200

    Add setting, import Obj_Yasi

---
+++ packages/core/src/mm/ovl/actors/Obj_Yasi/Obj_Yasi.h
@@ -0,0 +1,14 @@
+#ifndef OVL_OBJ_YASI_H
+#define OVL_OBJ_YASI_H
+
+#include <combo.h>
+
+struct ObjYasi;
+
+typedef struct ObjYasi {
+    /* 0x000 */ DynaPolyActor dyna;
+} ObjYasi; // size = 0x15C
+
+#define PALM_TREE_IS_WIDE(thisx) ((thisx)->params & 1)
+
+#endif
