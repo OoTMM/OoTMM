505519c0c64f5ea314bf3deefb3dc56ee1959139
Nax <4265415+Nax@users.noreply.github.com>
2025-12-02 18:45:55 +0100

    Getting the generator ready

--- packages/core/lib/combo/logic/world-transform.ts
+++ packages/core/lib/combo/logic/world-transform.ts
@@ -1414,10 +1414,12 @@ export class LogicPassWorldTransform {
     if (settings.soulsEnemyOot) this.addItems(ItemGroups.OOT_SOULS_ENEMY);
     if (settings.soulsBossOot) this.addItems(ItemGroups.OOT_SOULS_BOSS);
     if (settings.soulsNpcOot) this.addItems(ItemGroups.OOT_SOULS_NPC);
+    if (settings.soulsAnimalOot) this.addItems(ItemGroups.OOT_SOULS_ANIMAL);
     if (settings.soulsMiscOot) this.addItems(ItemGroups.OOT_SOULS_MISC);
     if (settings.soulsEnemyMm) this.addItems(ItemGroups.MM_SOULS_ENEMY);
     if (settings.soulsBossMm) this.addItems(ItemGroups.MM_SOULS_BOSS);
     if (settings.soulsNpcMm) this.addItems(ItemGroups.MM_SOULS_NPC);
+    if (settings.soulsAnimalMm) this.addItems(ItemGroups.MM_SOULS_ANIMAL);
     if (settings.soulsMiscMm) this.addItems(ItemGroups.MM_SOULS_MISC);
 
     /* Add skeleton keys */
