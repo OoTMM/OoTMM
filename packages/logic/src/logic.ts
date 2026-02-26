0c06e4065f1b32fde6f1cde311c5f60972f1e46b
Nax <4265415+Nax@users.noreply.github.com>
2026-02-26 21:49:51 +0100

    Cleaner logic index

---
+++ packages/logic/src/logic.ts
@@ -0,0 +1,91 @@
+import { Monitor, Random } from '@ootmm/core';
+
+/* TODO: extract this */
+import { Options } from '../../generator/lib/combo/options';
+
+import { LogicPassSolver } from './solve';
+import { LogicPassWorld } from './world';
+import { LogicPassSpoiler } from './spoiler';
+import { LogicPassHints } from './hints';
+import { LogicPassAnalysis } from './analysis';
+import { LogicPassEntrances } from './entrance';
+import { LogicPassHash } from './hash';
+import { LogicPassConfig } from './config';
+import { LogicPassWorldTransform } from './world-transform';
+import { LogicPassFixer } from './fixer';
+import { LogicPassAnalysisFoolish } from './analysis-foolish';
+import { LogicPassPrice } from './price';
+import { LogicPassItemProperties } from './item-properties';
+import { LogicPassMinimize } from './minimize';
+import { LogicPassAnalysisPaths } from './analysis-path';
+import { LogicPassSongEvents } from './song-events';
+
+export { locationList } from './location-list';
+
+interface LogicPass<Out> {
+  run: () => Out;
+}
+
+type LogicPassConstructor<In, Out> = new (state: In) => LogicPass<Out>;
+
+class LogicPipeline<State> {
+  constructor(private readonly state: State) {
+  }
+
+  apply<Out>(pass: LogicPassConstructor<State, Out>): LogicPipeline<State & Out> {
+    const passInstance = new pass(this.state);
+    const newState = { ...this.state, ...passInstance.run() };
+    return new LogicPipeline(newState);
+  }
+
+  exec(): State {
+    return this.state;
+  }
+};
+
+function pipeline<State>(state: State): LogicPipeline<State> {
+  return new LogicPipeline(state);
+}
+
+export const worldState = async (monitor: Monitor, opts: Options) => {
+  const random = new Random();
+  const seedKey = [opts.seed.length.toString(16), opts.seed, opts.settings.generateSpoilerLog ? '1' : '0'].join('\0');
+  await random.seed(seedKey);
+  const state = { monitor, opts, settings: opts.settings, random, attempts: 0 };
+
+  return pipeline(state)
+    .apply(LogicPassConfig)
+    .apply(LogicPassItemProperties)
+    .apply(LogicPassWorld)
+    .apply(LogicPassFixer)
+    .apply(LogicPassWorldTransform)
+    .exec();
+};
+
+export const solvedWorldState = async (monitor: Monitor, opts: Options) => {
+  let state = await worldState(monitor, opts);
+  return pipeline(state)
+    .apply(LogicPassPrice)
+    .apply(LogicPassSongEvents)
+    .apply(LogicPassEntrances)
+    .apply(LogicPassSolver)
+    .apply(LogicPassMinimize)
+    .exec();
+}
+
+export const logic = async (monitor: Monitor, opts: Options) => {
+  const state = await solvedWorldState(monitor, opts);
+
+  const data = pipeline(state)
+    .apply(LogicPassAnalysis)
+    .apply(LogicPassAnalysisPaths)
+    .apply(LogicPassAnalysisFoolish)
+    .apply(LogicPassHints)
+    .apply(LogicPassSpoiler)
+    .apply(LogicPassHash)
+    .exec();
+
+    return data;
+};
+
+export type LogicResult = Awaited<ReturnType<typeof logic>>;
