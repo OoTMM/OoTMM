3f432ca33fb3c7956a586c4448b1062b0c729b81
Nax <4265415+Nax@users.noreply.github.com>
2026-04-07 20:08:46 +0200

    Add donation link (web)

--- packages/web/src/app/layout/Header.tsx
+++ packages/web/src/app/layout/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitch } from '../theme/ThemeSwitch';
 
 export function Header() {
 	return (
-		<header className="flex items-center h-16 bg-gradient-to-r from-blue-900 to-purple-900 text-white">
+		<header className="flex items-center h-16 bg-linear-to-r from-blue-900 to-purple-900 text-white">
       <a href="/" className="flex items-center nav-link">
         <img className="rounded-2xl h-16 object-contain p-1 ml-3 mr-1" alt="OoTMM Logo" src={logo}/>
         <h1 className="text-4xl font-bold">OoTMM</h1>
@@ -13,8 +13,9 @@ export function Header() {
         <a className="nav-link" href="/multiplayer">Multiplayer Guide</a>
         <a className="nav-link" target="_blank" href="https://discord.gg/4QdtPBP6wf">Discord</a>
         <a className="nav-link" target="_blank" href="https://ootmm.wiki.gg">Wiki</a>
+        <a className="nav-link" target="_blank" href="https://opencollective.com/ootmm">Donate</a>
       </nav>
-      <div className="flex-grow"></div>
+      <div className="grow"></div>
       <div className="mr-8">
         <ThemeSwitch/>
       </div>
