fb701af8988b9ad31b83c3ae27cd48aad1f3308b
Nax <4265415+Nax@users.noreply.github.com>
2026-07-31 12:10:57 +0200

    Very, very early system in place

---
+++ packages/logic/src/solver/types.ts
@@ -0,0 +1,33 @@
+import type { Monitor, PlayerItem, PlayerItems, Random, Settings } from '@ootmm/core';
+import type { World } from '../world';
+import type { ItemProperties } from '../item-properties';
+import type { Location, ItemPlacement } from '../types';
+
+export type LogicPassSolverState = {
+  fixedLocations: Set<Location>;
+  worlds: World[];
+  settings: Settings;
+  random: Random;
+  monitor: Monitor;
+  pool: PlayerItems;
+  renewableJunks: PlayerItems;
+  startingItems: PlayerItems;
+  itemProperties: ItemProperties;
+  plandoLocations: Map<Location, PlayerItem>;
+};
+
+export type ItemPools = {
+  extra: PlayerItems,
+  required: PlayerItems,
+  nice: PlayerItems,
+  junk: PlayerItems,
+  nothing: PlayerItems,
+};
+
+export type SolverState = {
+  startingItems: PlayerItems;
+  items: ItemPlacement;
+  pools: ItemPools;
+  criticalRenewables: Set<PlayerItem>;
+  placedCount: number;
+};
