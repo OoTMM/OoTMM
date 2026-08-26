9cb4eec189022abd3ea8713c74aed8e964922cd9
Nax <4265415+Nax@users.noreply.github.com>
2026-08-26 23:02:00 +0200

    Alter pathfind, fix special conds

--- packages/generator/src/common/special_conds.c
+++ packages/generator/src/common/special_conds.c
@@ -366,6 +366,7 @@ int SpecialConds_Eval(int special)
 
         if (gMmSave.info.inventory.items[ITS_MM_MASK_DEKU] == ITEM_MM_MASK_DEKU) count++;
         if (gMmExtraItems.fierceDeityAdult & (1 << 0)) count++;
+        if (gMmExtraItems.fierceDeityAdult & (1 << 1)) count++;
     }
 
     if (cond->flags & SPF_MASKS_OOT)
