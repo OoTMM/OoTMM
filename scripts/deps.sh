2288495f256dc94df2b1b1d10c67bd806aa52ef9
Nax <4265415+Nax@users.noreply.github.com>
2026-08-27 19:29:11 +0200

    More CI stuff

---
+++ scripts/deps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+if [ ! -d "third_party/ultralib" ]; then
+  mkdir -p third_party
+  git clone --depth=1 https://github.com/decompals/ultralib third_party/ultralib
+fi
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-if [ ! -d "third_party/ultralib" ]; then
-  mkdir -p third_party
-  git clone --depth=1 https://github.com/decompals/ultralib third_party/ultralib
-fi
-
+./scripts/deps.sh
 pnpm install
