ecbbad3024b246fecf6631149f56e819a05c6c66
Nax <4265415+Nax@users.noreply.github.com>
2026-09-03 19:02:52 +0200

    Import MM En_Tubo_Trap, also fix a vanilla bug where they would not play the correct SFX

--- packages/generator/src/oot/ovl/actors/En_Tubo_Trap/En_Tubo_Trap.c
+++ packages/generator/src/oot/ovl/actors/En_Tubo_Trap/En_Tubo_Trap.c
@@ -1,6 +1,5 @@
 #include "En_Tubo_Trap.h"
 #include <assets/oot/objects/gameplay_dangeon_keep.h>
-//#include "overlays/effects/ovl_Effect_Ss_Kakera/z_eff_ss_kakera.h"
 
 #define FLAGS ACTOR_FLAG_UPDATE_CULLING_DISABLED
 
