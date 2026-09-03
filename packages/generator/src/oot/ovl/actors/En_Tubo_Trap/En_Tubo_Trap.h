05a598d117c0a70988f4cce578422834ebcf4c0d
Nax <4265415+Nax@users.noreply.github.com>
2026-09-03 18:56:37 +0200

    Import OoT Tubo_Trap OVL

---
+++ packages/generator/src/oot/ovl/actors/En_Tubo_Trap/En_Tubo_Trap.h
@@ -0,0 +1,18 @@
+#ifndef Z_EN_TUBO_TRAP_H
+#define Z_EN_TUBO_TRAP_H
+
+#include <combo.h>
+
+struct EnTuboTrap;
+
+typedef void (*EnTuboTrapActionFunc)(struct EnTuboTrap*, struct PlayState*);
+
+typedef struct EnTuboTrap {
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ EnTuboTrapActionFunc actionFunc;
+    /* 0x0150 */ f32 targetY;
+    /* 0x0154 */ Vec3f originPos;
+    /* 0x0160 */ ColliderCylinder collider;
+} EnTuboTrap; // size = 0x01AC
+
+#endif
